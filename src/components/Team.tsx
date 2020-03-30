729e2ea4a37f2d0f1d50e603c06b09bdca43e939
Maurice Faber <morriz@idiotz.nl>
2020-03-30 12:37:05 +0200

    fix: added submit button, but it's not disabling yet on validation error

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,64 +1,60 @@
-import { RuleEffect } from '@jsonforms/core';
-import {materialCells, materialRenderers} from '@jsonforms/material-renderers';
-import { JsonForms } from '@jsonforms/react';
+import { RuleEffect } from '@jsonforms/core'
+import { materialCells, materialRenderers } from '@jsonforms/material-renderers'
+import { JsonForms } from '@jsonforms/react'
 
-import React from 'react'
+import { Button } from '@material-ui/core'
+import React, { useState } from 'react'
 import Form from 'react-jsonschema-form-bs4'
 import { getSchema } from '../hooks/api'
-import CustomDescriptionField from './CustomDescriptionField'
-
-const fields = {
-  DescriptionField: CustomDescriptionField,
-}
 
 export default ({ onSubmit, clusters, team = {} }): any => {
+  const myTeam = team === null ? undefined : team
+  const [formData, setFormData] = useState(myTeam)
+  const [submitActive, setSubmitActive] = useState(false)
+  const handleChange = ({ errors, data }): any => {
+    if ((!errors || !errors.length) && data) {
+      setSubmitActive(true)
+    }
+  }
   const handleSubmit = (form): any => {
-    onSubmit(form.formData)
+    onSubmit(formData)
   }
   const schema = getSchema()
   const mySchema = schema.getTeamSchema(clusters)
-  const uiSchema = schema.getTeamUiSchema(mySchema)
-  console.log(mySchema);
+  console.log(mySchema)
   const form = {
-    "type": "VerticalLayout",
-    "elements": [
+    type: 'VerticalLayout',
+    elements: [
       {
-        "type": "Control",
-        "scope": "#/properties/name"
+        type: 'Control',
+        scope: '#/properties/name',
       },
       {
-        "type": "Control",
-        "scope": "#/properties/clusters",
+        type: 'Control',
+        scope: '#/properties/clusters',
 
         options: {
-          detail: 'DEFAULT'
-        }
-      }
-    ]
+          detail: 'DEFAULT',
+        },
+      },
+    ],
   }
 
   return (
     <div className='Team'>
       <h2>Team details:</h2>
-      
+
       <JsonForms
         schema={mySchema}
+        onChange={handleChange}
         uischema={form}
-        data={team}
+        data={myTeam}
         renderers={materialRenderers}
         cells={materialCells}
       />
-      
-      <Form
-        key='createTeam'
-        schema={mySchema}
-        fields={fields}
-        uiSchema={uiSchema}
-        onSubmit={handleSubmit}
-        onError={console.error}
-        formData={team}
-        // liveValidate={true}
-      />
+      <Button color='primary' onClick={handleSubmit} disabled={!submitActive}>
+        Submit
+      </Button>
     </div>
   )
 }
