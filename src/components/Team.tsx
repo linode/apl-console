d8e23e4503780f74432f3aaf808a637d4e87ae57
Jehoszafat Zimnowoda <jzimnowo@gmail.com>
2020-03-30 23:53:13 +0200

    feat: use react-json-forms with material-ui theme

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,46 +1,39 @@
-import { materialCells, materialRenderers } from '@jsonforms/material-renderers'
-import { JsonForms } from '@jsonforms/react'
+
 import { Button } from '@material-ui/core'
-import { isEmpty } from 'lodash/lang'
-import React, { useState } from 'react'
+import React from 'react'
+import { withTheme } from 'react-jsonschema-form';
+import { Theme as MuiTheme } from 'rjsf-material-ui';
 import { getSchema } from '../hooks/api'
-import HiddenFieldRender from '../utils/jsonforms/HiddenFieldRender'
-import HiddenFieldTesterFactory from '../utils/jsonforms/HiddenFieldTesterFactory'
+
+const Form = withTheme(MuiTheme);
 
 export default ({ onSubmit, clusters, team = {} }): any => {
   const myTeam = team === null ? undefined : team
-  const [formData, setFormData] = useState(myTeam)
-  const [submitActive, setSubmitActive] = useState(false)
-  const handleChange = ({ errors, data }): any => {
-    if (isEmpty(errors) && !isEmpty(data)) {
-      setSubmitActive(true)
-    } else {
-      setSubmitActive(false)
-    }
-    setFormData(data)
-  }
-  const handleSubmit = (): any => {
+  const handleSubmit = ({schema, uiSchema, formData, edit, errors}): any => {
     onSubmit(formData)
   }
   const schema = getSchema()
   const mySchema = schema.getTeamSchema(clusters)
-
-  const HiddenFieldsTester = HiddenFieldTesterFactory(['teamId', 'serviceId', 'dummyProperty', 'password'])
+  const uiSchema = schema.getTeamUiSchema(mySchema)
 
   return (
     <div className='Team'>
       <h2>Team details:</h2>
 
-      <JsonForms
+      <Form
+        key='createTeam'
         schema={mySchema}
-        onChange={handleChange}
-        data={formData}
-        renderers={[...materialRenderers, { tester: HiddenFieldsTester, renderer: HiddenFieldRender }]}
-        cells={materialCells}
-      />
-      <Button color='primary' onClick={handleSubmit} disabled={!submitActive}>
-        Submit
-      </Button>
+        uiSchema={uiSchema}
+        // onChange={handleChange}
+        onSubmit={handleSubmit}
+        formData={myTeam}
+        liveValidate={false}
+        showErrorList={false}
+      >
+        <Button color='primary' type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   )
 }
