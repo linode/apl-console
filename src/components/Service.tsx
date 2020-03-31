3240292dac8d5f3bbb892adfb927cf7e4fed7af3
Jehoszafat Zimnowoda <jzimnowo@gmail.com>
2020-03-31 09:36:07 +0200

    feat: use react-json-forms with material-ui theme for service form

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -1,51 +1,34 @@
-import { JsonFormsCore } from '@jsonforms/core';
-import {materialCells, materialRenderers} from '@jsonforms/material-renderers';
-import { JsonForms } from '@jsonforms/react';
-import { Button } from '@material-ui/core';
-import { isEmpty } from 'lodash/lang'
-import React, { useState } from 'react'
+import { Button } from '@material-ui/core'
+import React from 'react'
+import { withTheme } from 'react-jsonschema-form'
+import { Theme as MuiTheme } from 'rjsf-material-ui'
 import { getSchema } from '../hooks/api'
-// import EmptyDictRender from '../utils/jsonforms/EmptyDictRender'
-// import EmptyDictTester from '../utils/jsonforms/EmptyDictTester'
-import HiddenFieldRender from '../utils/jsonforms/HiddenFieldRender'
-import HiddenFieldTesterFactory from '../utils/jsonforms/HiddenFieldTesterFactory'
+
+const Form = withTheme(MuiTheme)
 
 export default ({ onSubmit, clusters, service = {} }): any => {
-  const [formData, setFormData] = useState(service)
-  const [submitActive, setSubmitActive] = useState(false)
-
-  const handleChange = (state: Pick<JsonFormsCore, 'data' | 'errors'>): any => {
-    if (isEmpty(state.errors) && !isEmpty(state.data)) {
-      setSubmitActive(true)
-      setFormData(state.data)
-    }
-  }
-  const handleSubmit = (): any => {
+  const handleSubmit = ({ schema, uiSchema, formData, edit, errors }): any => {
     onSubmit(formData)
   }
-
   const schema = getSchema()
   const mySchema = schema.getServiceSchema(clusters)
-
-
-  const HiddenFieldsTester = HiddenFieldTesterFactory(['teamId', 'serviceId', 'dummyProperty'])
+  const uiSchema = schema.getServiceUiSchema(mySchema)
   return (
     <div className='Service'>
       <h2>Service:</h2>
-      <JsonForms
+      <Form
+        key='createTeam'
         schema={mySchema}
-        data={formData}
-        renderers={[
-          ...materialRenderers,
-          // { tester: EmptyDictTester, renderer: EmptyDictRender },
-          { tester: HiddenFieldsTester, renderer: HiddenFieldRender }
-        ]}
-        cells={materialCells}
-        onChange={handleChange}
-      />
-      <Button color='primary' onClick={handleSubmit} disabled={!submitActive}>
-        Submit
-      </Button>
+        uiSchema={uiSchema}
+        onSubmit={handleSubmit}
+        formData={service}
+        liveValidate={false}
+        showErrorList={true}
+      >
+        <Button color='primary' type='submit'>
+          Submit
+        </Button>
+      </Form>
     </div>
   )
 }
