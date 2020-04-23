9024cada3d01ae597bdc53735f0cdd22eebabd1b
Jehoszafat Zimnowoda <jzimnowo@gmail.com>
2020-04-23 15:04:56 +0200

    feat: display boolean properites in one row

--- src/components/FormTemplate.tsx
+++ src/components/FormTemplate.tsx
@@ -4,9 +4,11 @@ import React from 'react'
 export default function ObjectFieldTemplate(props: any) {
   const { title, description, properties, idSchema } = props
   const simpleElements = []
+  const booleanElements = []
   const complexElements = []
   properties.forEach(o => {
     if (o.content.props.schema.type === 'object' || o.content.props.schema.type === 'array') complexElements.push(o)
+    else if (o.content.props.schema.type === 'boolean') booleanElements.push(o)
     else simpleElements.push(o)
   })
 
@@ -22,6 +24,11 @@ export default function ObjectFieldTemplate(props: any) {
 
         <Grid container spacing={3} direction='row' justify='flex-start' alignItems='flex-start'>
           {simpleElements.map(element => (
+            <Grid item>{element.content}</Grid>
+          ))}
+        </Grid>
+        <Grid container spacing={3} direction='row' justify='flex-start' alignItems='flex-start'>
+          {booleanElements.map(element => (
             <Grid item>{element.content}</Grid>
           ))}
         </Grid>
