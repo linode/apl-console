20a85716e01a50630e41e4901bc4c841835c3ccb
Riza Nugraha <rnugraha@gmail.com>
2020-05-04 18:49:08 +0200

    feat(dashboard.tsx): initialize dashboard cards
    #31

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,34 +1,108 @@
-import { Box, Button, Container, Typography } from '@material-ui/core'
-import AddIcon from '@material-ui/icons/Add'
+import { Typography, Grid, Card, CardHeader, Avatar, IconButton, makeStyles, Divider } from '@material-ui/core'
 import * as React from 'react'
-import { Link } from 'react-router-dom'
+import AddCircleIcon from '@material-ui/icons/Add'
+import SwapVerticalCircleIcon from '@material-ui/icons/SwapVerticalCircle'
+import PeopleIcon from '@material-ui/icons/People'
+import CloudIcon from '@material-ui/icons/Cloud'
 import { Team } from '../models'
 
 interface Props {
   team?: Team
 }
 
+const useStyles = makeStyles(theme => ({
+  card: {
+    backgroundColor: theme.palette.primary.light,
+    color: theme.palette.primary.contrastText,
+    boxShadow: 'none',
+  },
+  title: {
+    paddingTop: 30,
+    paddingBottom: 20,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    // backgroundColor: red[500],
+  },
+}))
+
 const Dashboard = ({ team }: Props): any => {
+  const classes = useStyles()
+
   return (
-    <Container maxWidth='xs'>
-      <Box justifyContent='center' display='flex' alignItems='center' textAlign='center'>
-        <Typography variant='h3'>
-          Welcome to the team <b>{team ? team.name : 'Admin'}</b> dashboard!
-        </Typography>
-        {team && (
-          <Button
-            variant='contained'
-            color='primary'
-            size='large'
-            component={Link}
-            to={`/teams/${team.teamId}/create-service`}
-            startIcon={<AddIcon />}
-          >
-            Create a new Service!
-          </Button>
-        )}
-      </Box>
-    </Container>
+    <>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typography variant='h5' gutterBottom className={classes.title}>
+            Welcome to the team <b>{team ? team.name : 'Admin'}</b> dashboard!
+          </Typography>
+          <Divider />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Card classes={{ root: classes.card }}>
+            <CardHeader
+              avatar={
+                <Avatar aria-label='recipe' className={classes.avatar}>
+                  <CloudIcon />
+                </Avatar>
+              }
+              title='Clusters'
+              subheader='6' // TODO
+            />
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={3}>
+          <Card classes={{ root: classes.card }}>
+            <CardHeader
+              avatar={
+                <Avatar aria-label='recipe' className={classes.avatar}>
+                  <PeopleIcon />
+                </Avatar>
+              }
+              title='Teams'
+              subheader='30' // TODO
+              action={
+                <IconButton aria-label='settings'>
+                  <AddCircleIcon />
+                </IconButton>
+              }
+            />
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Card classes={{ root: classes.card }}>
+            <CardHeader
+              avatar={
+                <Avatar aria-label='recipe' className={classes.avatar}>
+                  <SwapVerticalCircleIcon />
+                </Avatar>
+              }
+              action={
+                <IconButton aria-label='settings'>
+                  <AddCircleIcon />
+                </IconButton>
+              }
+              title='Services'
+              subheader='30' // TODO
+            />
+          </Card>
+        </Grid>
+      </Grid>
+    </>
   )
 }
 
