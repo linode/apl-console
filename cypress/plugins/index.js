0b7e22dc27715c72de9e9cf39c743b9b735c2ce0
Riza Nugraha <rnugraha@gmail.com>
2020-05-13 18:20:36 +0200

    feat: add config for Percy to enabe snapshot test

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,11 +11,13 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-
+// In cypress/plugins/index.js
+let percyHealthCheck = require('@percy/cypress/task')
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  on("task", percyHealthCheck);
 }
