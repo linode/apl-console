5290140773d11bf9f2f9e96a018a2b50bbd59945
Riza Nugraha <rnugraha@gmail.com>
2020-05-14 15:36:00 +0200

    feat(dashboard): redirect user to the list page when clicking the info

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,12 +1,19 @@
-import { Typography, Grid, Card, CardHeader, Avatar, IconButton, makeStyles, Divider } from '@material-ui/core'
+import { Typography, Grid, Card, CardHeader, Avatar, IconButton, makeStyles, Divider, Tooltip } from '@material-ui/core'
 import * as React from 'react'
 import AddCircleIcon from '@material-ui/icons/Add'
 import SwapVerticalCircleIcon from '@material-ui/icons/SwapVerticalCircle'
 import PeopleIcon from '@material-ui/icons/People'
 import CloudIcon from '@material-ui/icons/Cloud'
-import { Link } from 'react-router-dom'
+import Link from '@material-ui/core/Link';
+import { Link as RouterLink} from 'react-router-dom'
 import { Team } from '../models'
 
+type Panel = {
+  name: string;
+  data: any;
+  icon: any;
+  canCreate: boolean;
+}
 interface Props {
   team?: Team
   isAdmin?: boolean
@@ -54,77 +61,67 @@ const useStyles = makeStyles(theme => ({
     transform: 'rotate(180deg)',
   },
 }))
+interface DashboardCardProps {
+  item: Panel
+  teamId?: string
+  classes: any
+}
+
+const DashboardCard = ({classes, teamId, item }: DashboardCardProps) => {
+  const prefix = teamId ? `/teams/${teamId}` : ''
+  return (
+    <Grid item xs={12} sm={3}>
+      <Card classes={{ root: classes.card }}>
+        <CardHeader
+          classes={{ subheader: classes.cardSubHeader }}
+          avatar={
+            <Avatar aria-label='recipe'>
+              {item.icon}
+            </Avatar>
+          }
+          title={item.name}
+          subheader={(
+            <Link 
+              component={RouterLink} 
+              to={item.name === 'service' ? `${prefix}/${item.name}s` : `/${item.name}s`}>
+                {item.data && item.data.length}
+            </Link>
+          )}
+          action={ item.canCreate &&
+            <Tooltip title={`Create ${item.name}`} aria-label={`create ${item.name}`}>
+              <IconButton aria-label='settings' component={RouterLink} to={`${prefix}/create-${item.name}`}>
+                <AddCircleIcon />
+              </IconButton>
+            </Tooltip>            
+          }
+        />
+      </Card>
+    </Grid>
+  )
+}
 
 const Dashboard = ({ team, data: { services, clusters, teams }, isAdmin }: Props): any => {
   const classes = useStyles()
+  const panels = [
+    {name: 'cluster', data: clusters, icon:  <CloudIcon />, canCreate: false}, 
+    {name: 'team', data: teams, icon:  <PeopleIcon />, canCreate: isAdmin }, 
+    {name: 'service', data: services, icon:  <SwapVerticalCircleIcon />, canCreate: true}, 
+  ]
   return (
     <>
-      <Grid container spacing={3}>
+      <Grid container spacing={3}>        
         <Grid item xs={12}>
           <Typography variant='h5' gutterBottom className={classes.title}>
             Welcome to the team <b className={classes.teamName}>{team ? team.name : 'Admin'}</b> dashboard!
           </Typography>
           <Divider />
         </Grid>
-        <Grid item xs={12} sm={3}>
-          <Card classes={{ root: classes.card }}>
-            <CardHeader
-              classes={{ subheader: classes.cardSubHeader }}
-              avatar={
-                <Avatar aria-label='recipe'>
-                  <CloudIcon />
-                </Avatar>
-              }
-              title='Clusters'
-              subheader={clusters && clusters.length}
-            />
-          </Card>
-        </Grid>
-
-        <Grid item xs={12} sm={3}>
-          <Card classes={{ root: classes.card }}>
-            <CardHeader
-              classes={{ subheader: classes.cardSubHeader }}
-              avatar={
-                <Avatar aria-label='recipe'>
-                  <PeopleIcon />
-                </Avatar>
-              }
-              title='Teams'
-              subheader={teams && teams.length}
-              action={
-                isAdmin && (
-                  <IconButton aria-label='settings' component={Link} to='/create-team'>
-                    <AddCircleIcon />
-                  </IconButton>
-                )
-              }
-            />
-          </Card>
-        </Grid>
-        <Grid item xs={12} sm={3}>
-          <Card classes={{ root: classes.card }}>
-            <CardHeader
-              classes={{ subheader: classes.cardSubHeader }}
-              avatar={
-                <Avatar aria-label='recipe'>
-                  <SwapVerticalCircleIcon />
-                </Avatar>
-              }
-              action={
-                <IconButton
-                  aria-label='settings'
-                  component={Link}
-                  to={isAdmin ? '/create-service' : `/teams/${team.teamId}/create-service`}
-                >
-                  <AddCircleIcon />
-                </IconButton>
-              }
-              title='Services'
-              subheader={services && services.length}
-            />
-          </Card>
-        </Grid>
+        {panels.map(panel => <DashboardCard 
+          classes={classes} 
+          teamId={team && team.teamId} 
+          item={panel}
+          key={panel.name}/>
+        )}
       </Grid>
     </>
   )
