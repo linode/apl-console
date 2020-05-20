5a8718fac5945ef38ce8b093c8a284b0f7a36729
Riza Nugraha <rnugraha@gmail.com>
2020-05-20 17:27:01 +0200

    chore: use cypress selector

--- cypress/integration/otomi-web/init_app.js
+++ cypress/integration/otomi-web/init_app.js
@@ -1,11 +1,18 @@
 describe('Otomi Web', () => {
-
     beforeEach(() => {
         cy.visit('localhost:3000')
     })
+    it('should load Otomi Web Admin Dashboard', () => {
+        cy.get('.MuiGrid-container > :nth-child(1) > .MuiTypography-root')
+            .contains('Welcome to the team Admin dashboard')
+        cy.percySnapshot()
+    })
 
-    it('should load Otomi Web app', () => {
-        cy.title().should('eq', 'Otomi')
+    it('should go to create team page', () => {
+        cy.get('[data-cy="cy-create-team-btn"]').click()
+        cy.location().should(loc => {
+            expect(loc.pathname).to.eq('/create-team')
+        })
         cy.percySnapshot()
     })
 })
