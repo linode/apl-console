868aeadbd907b6be030d7d859e46cc39b2e5a5dd
Maurice Faber <maurice.faber@redkubes.com>
2020-10-24 21:19:15 +0200

    improvement: locked menu items, enriched settings

--- src/components/ChannelSelector.tsx
+++ src/components/ChannelSelector.tsx
@@ -8,7 +8,7 @@ import {
 } from '@material-ui/core'
 import React from 'react'
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(() => ({
   labelChecked: {},
   lastItem: {
     marginRight: 0,
