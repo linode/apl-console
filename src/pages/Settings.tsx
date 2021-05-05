291cfa15abb2a61d4334ff32c51a787d34780cc6
svatwork <ajsverbeek@gmail.com>
2021-05-05 08:47:35 +0200

    feat: can do expression without assignment

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -3,6 +3,5 @@ import PaperLayout from '../layouts/Paper'
 import Settings from '../components/Settings'
 
 export default (): React.ReactElement => {
-  const comp = <Settings someProp={1} />
-  return <PaperLayout comp={comp} />
+  return <PaperLayout comp={<Settings someProp={1} />} />
 }
