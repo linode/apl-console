2abf95bb9cde3bf7966d19881e2cf763673f60ae
svatwork <ajsverbeek@gmail.com>
2021-05-07 13:54:02 +0200

    feat: add back settings version form

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import PaperLayout from '../layouts/Paper'
 import SettingsForm from '../components/SettingsForm'
 import { useApi } from '../hooks/api'
+import SettingsVersionForm from '../components/SettingsVersionForm'
 
 export default (): React.ReactElement => {
   const [getFormData, getLoading, getErr] = useApi('getSettings')
@@ -16,7 +17,14 @@ export default (): React.ReactElement => {
     <PaperLayout
       err={err}
       loading={loading}
-      comp={!(err || loading) && <SettingsForm setFormData={setFormData} formData={getFormData} />}
+      comp={
+        !(err || loading) && (
+          <>
+            <SettingsVersionForm />
+            <SettingsForm setFormData={setFormData} formData={getFormData} />
+          </>
+        )
+      }
     />
   )
 }
