583dc1f91a331f7216be742ed15dec19d85acb7a
Maurice Faber <maurice.faber@redkubes.com>
2021-05-22 18:58:37 +0200

    fix: globlError, cluster selector

--- src/components/Clusters.tsx
+++ src/components/Clusters.tsx
@@ -1,39 +1,40 @@
 import React from 'react'
-import { Settings } from '@redkubes/otomi-api-client-axios'
+import { Cluster } from '@redkubes/otomi-api-client-axios'
 import MuiLink from './MuiLink'
 import EnhancedTable, { HeadCell } from './EnhancedTable'
+import { useSession } from '../session-context'
 
-interface Props {
-  settings: Settings
-}
-
-export default ({ settings }: Props): React.ReactElement => {
+export default (): React.ReactElement => {
+  const { cluster, clusters } = useSession()
+  const allClusters = [...clusters, cluster]
   const headCells: HeadCell[] = [
     {
-      id: 'url',
-      label: 'Url',
-      renderer: (cluster: any) => {
-        const { url, name } = cluster
-        return (
-          <MuiLink href={url} target='_blank' rel='noopener'>
-            {name}
-          </MuiLink>
-        )
-      },
+      id: 'provider',
+      label: 'Provider',
     },
     {
       id: 'name',
       label: 'Name',
     },
     {
-      id: 'provider',
-      label: 'Provider',
+      id: 'url',
+      label: 'Url',
+      renderer: (c: Cluster) => {
+        const { domainSuffix } = c
+        const domain = `otomi.${domainSuffix}`
+        if (domainSuffix === cluster.domainSuffix) return domain
+        return (
+          <MuiLink href={`https://${domain}`} rel='noopener'>
+            {domain}
+          </MuiLink>
+        )
+      },
     },
   ]
   return (
     <>
       <h1 data-cy='h1-clusters-page'>Clusters</h1>
-      <EnhancedTable disableSelect headCells={headCells} orderByStart='name' rows={settings.clusters} idKey='id' />
+      <EnhancedTable disableSelect headCells={headCells} orderByStart='name' rows={allClusters} idKey='id' />
     </>
   )
 }
