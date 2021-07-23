7a0c4935980b02a377f701c21b401e79389427b2
svatwork <ajsverbeek@gmail.com>
2021-07-23 10:58:23 +0200

    fix: wrangle demo values

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-nested-ternary */
 import { Box, Button } from '@material-ui/core'
 import AddCircleIcon from '@material-ui/icons/AddCircle'
 import React from 'react'
@@ -37,33 +38,35 @@ interface Props {
   team?: Team
 }
 
+// TODO: https://github.com/redkubes/otomi-core/discussions/475
 export default ({ services, team }: Props): React.ReactElement => {
   const {
     mode,
     user: { isAdmin },
     oboTeamId,
   } = useSession()
-  const showTeam = !team
+  const isCE = mode === 'ce'
+  const showTeam = isCE ? false : !team
   const headCells: HeadCell[] = [
     {
       id: 'name',
       label: 'Service Name',
-      renderer: (row) => (mode === 'ce' ? row.name : getServiceLink(isAdmin, oboTeamId)),
+      renderer: (row) => (isCE ? row.name : getServiceLink(isAdmin, oboTeamId)),
     },
     {
       id: 'ingressType',
       label: 'Ingress',
-      renderer: (row) => row.ingress?.type ?? '',
+      renderer: (row) => (isCE ? row.type : row.ingress?.type ?? ''),
     },
     {
       id: 'serviceType',
       label: 'Type',
-      renderer: (row) => row.ksvc?.serviceType ?? '',
+      renderer: (row) => (isCE ? ('ksvc' in row ? 'ksvc' : 'regular') : row.ksvc?.serviceType ?? ''),
     },
     {
       id: 'host',
-      label: 'Host Name',
-      renderer: renderHost,
+      label: isCE ? 'Port' : 'Host Name',
+      renderer: (row) => (isCE ? `:${row.port}` : renderHost),
       component: MuiLink,
     },
   ]
@@ -84,7 +87,7 @@ export default ({ services, team }: Props): React.ReactElement => {
             startIcon={<AddCircleIcon />}
             variant='contained'
             color='primary'
-            disabled={isAdmin && !oboTeamId}
+            disabled={isCE || (isAdmin && !oboTeamId)}
             data-cy='button-create-service'
           >
             Create service
