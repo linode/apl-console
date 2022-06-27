e3a7e2e69ce756ba147d8840bc28844d70a803a6
Maurice Faber <morriz@idiotz.nl>
2022-06-27 14:03:03 +0200

    fix: password hiding

--- src/components/rjsf/StringField.tsx
+++ src/components/rjsf/StringField.tsx
@@ -11,6 +11,8 @@ export default function ({ children, schema, uiSchema, formData, placeholder, ..
   const listTooLong = schema.enum?.length > 7
   const shortList = schema.enum?.length < 5
   if (isHidden({ schema })) uiSchema['ui:widget'] = 'hidden'
+  console.log('schema: ', schema)
+  console.log('uiSchema: ', uiSchema)
   if (uiSchema['ui:widget'] !== 'hidden') {
     if (schema.enum && schema.enum.length === 1) {
       // hide one item enum that was set to its default value, as those are used for selectors
@@ -21,13 +23,15 @@ export default function ({ children, schema, uiSchema, formData, placeholder, ..
       newUiSchema['ui:widget'] = RadioWidget
       set(newUiSchema, 'ui:options.inline', shortList)
       set(newUiSchema, 'ui:options.hasLabel', true)
-    } else if (uiSchema['ui:widget'] !== 'hidden' && !!schema['x-secret'] && !schema['x-decrypt']) {
+    } else if (schema['x-secret'] !== undefined && !schema['x-decrypt']) {
       newUiSchema['ui:widget'] = 'password'
       set(newUiSchema, 'ui:options.autocomplete', 'off')
     } else if (schema['x-formtype']) newUiSchema['ui:widget'] = schema['x-formtype']
 
     if (renderedPlaceholder) newUiSchema['ui:placeholder'] = renderedPlaceholder
   }
+  console.log('newSchema: ', newSchema)
+  console.log('newUiSchema: ', newUiSchema)
   return (
     <StringField {...props} formData={formData} schema={newSchema} uiSchema={newUiSchema}>
       {children}
