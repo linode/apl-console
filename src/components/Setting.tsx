3ea297c4c48989b3cb2d59af2236591255d5322c
Ferruh Cihan <ferruh.cihan@redkubes.com>
2023-05-05 14:23:32 +0200

    feat: add dynamicUiSchema function for alerts settings

--- src/components/Setting.tsx
+++ src/components/Setting.tsx
@@ -143,14 +143,24 @@ export default function ({ settings: data, settingId, ...other }: Props): React.
       if (!appsEnabled.velero && settingId === 'backup') setDisabledMessage('Please enable Velero to activate Backups')
     }
   }, [settingId])
-
   // END HOOKS
+  const getDynamicUiSchema = (data) => {
+    const { receivers, drone } = schema.properties
+    const allItems = [...new Set([...receivers.items.enum, ...drone.items.enum])]
+    const uiSchema = {}
+    const diff = allItems.filter((receiver) => !data.receivers.includes(receiver) && !data.drone.includes(receiver))
+    diff.forEach((receiver) => {
+      uiSchema[receiver] = { 'ui:widget': 'hidden' }
+    })
+    return uiSchema
+  }
+
   const onChangeHandler = (data) => {
     const schema = getSettingSchema(appsEnabled, settings, settingId, data)
     setSetting(data)
     const uiSchema = getSettingUiSchema(appsEnabled, settings, settingId)
     setSchema(schema)
-    setUiSchema(uiSchema)
+    setUiSchema({ ...uiSchema, ...getDynamicUiSchema(data) })
   }
   return (
     <>
