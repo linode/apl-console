8aded7c3a373a347591ea90881654d2b78f5249f
Ferruh Cihan <ferruh.cihan@redkubes.com>
2023-06-05 14:39:31 +0200

    feat: added RadioButtons on workload values page

--- src/components/WorkloadEssentialValues.tsx
+++ src/components/WorkloadEssentialValues.tsx
@@ -3,13 +3,15 @@ import { applyAclToUiSchema, getSpec } from 'common/api-spec'
 import { cloneDeep } from 'lodash'
 import { CrudProps } from 'pages/types'
 import { useSession } from 'providers/Session'
-import React from 'react'
+import React, { useState } from 'react'
 import { GetSessionApiResponse } from 'redux/otomiApi'
+import { FormControl, FormControlLabel, Radio, RadioGroup } from '@mui/material'
 import Form from './rjsf/Form'
 
-export const getWorkloadValuesSchema = (selectedChart): any => {
+export const getWorkloadValuesSchema = (selectedChart, valuesType): any => {
   const schema: any = cloneDeep(getSpec().components.schemas.WorkloadValues)
   if (selectedChart === 'ksvc') schema.properties.values.properties.autoscaling.properties.minReplicas.default = 0
+  schema.properties.values.title = `${valuesType} values`
   return schema
 }
 
@@ -37,6 +39,8 @@ export const getWorkloadValuesUiSchema = (user: GetSessionApiResponse['user'], t
   return uiSchema
 }
 
+const workloadValuesTypes = ['Basic', 'Advanced']
+
 interface Props extends CrudProps {
   teamId: string
   valuesData: any
@@ -46,21 +50,35 @@ interface Props extends CrudProps {
 
 export default function ({ teamId, valuesData, setValuesData, selectedChart, ...other }: Props): React.ReactElement {
   const { appsEnabled, user } = useSession()
+  const [valuesType, setValuesType] = useState('Basic')
   // END HOOKS
-  const schema = getWorkloadValuesSchema(selectedChart)
+  const schema = getWorkloadValuesSchema(selectedChart, valuesType)
   const uiSchema = getWorkloadValuesUiSchema(user, teamId)
 
   return (
-    <Form
-      schema={schema}
-      uiSchema={uiSchema}
-      data={valuesData}
-      onChange={setValuesData}
-      disabled={!appsEnabled.argocd}
-      resourceType='Workload'
-      children
-      hideHelp
-      {...other}
-    />
+    <>
+      <FormControl sx={{ mt: 2 }}>
+        <RadioGroup
+          sx={{ display: 'flex', flexDirection: 'row' }}
+          onChange={(e) => setValuesType(e.target.value)}
+          value={valuesType}
+        >
+          {workloadValuesTypes.map((value: string) => (
+            <FormControlLabel key={value} value={value} control={<Radio />} label={`${value} values`} />
+          ))}
+        </RadioGroup>
+      </FormControl>
+      <Form
+        schema={schema}
+        uiSchema={uiSchema}
+        data={valuesData}
+        onChange={setValuesData}
+        disabled={!appsEnabled.argocd}
+        resourceType='Workload'
+        children
+        hideHelp
+        {...other}
+      />
+    </>
   )
 }
