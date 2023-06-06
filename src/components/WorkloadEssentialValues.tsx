327f8794793ae2b9696aba02955a565d0a2ceb11
Ferruh Cihan <ferruh.cihan@redkubes.com>
2023-06-06 08:54:51 +0200

    feat: update WorkloadEssentialValues component

--- src/components/WorkloadEssentialValues.tsx
+++ src/components/WorkloadEssentialValues.tsx
@@ -5,17 +5,44 @@ import { CrudProps } from 'pages/types'
 import { useSession } from 'providers/Session'
 import React, { useState } from 'react'
 import { GetSessionApiResponse } from 'redux/otomiApi'
-import { FormControl, FormControlLabel, Radio, RadioGroup } from '@mui/material'
+import { Box, FormControl, FormControlLabel, Radio, RadioGroup } from '@mui/material'
 import Form from './rjsf/Form'
 
-export const getWorkloadValuesSchema = (selectedChart, valuesType): any => {
+export const getWorkloadValuesSchema = (selectedChart: any, valuesType: string): any => {
   const schema: any = cloneDeep(getSpec().components.schemas.WorkloadValues)
-  if (selectedChart === 'ksvc') schema.properties.values.properties.autoscaling.properties.minReplicas.default = 0
+  const defaultServicePorts = [
+    {
+      name: 'http',
+      port: 80,
+      targetPort: 'http',
+      protocol: 'TCP',
+    },
+  ]
+  if (selectedChart === 'deployment') schema.properties.values.properties.servicePorts.default = defaultServicePorts
+  if (selectedChart === 'ksvc') {
+    schema.properties.values.properties.autoscaling.properties.minReplicas.default = 0
+    delete schema.properties.values.properties.servicePorts
+  }
   schema.properties.values.title = `${valuesType} values`
   return schema
 }
 
-export const getWorkloadValuesUiSchema = (user: GetSessionApiResponse['user'], teamId: string): any => {
+export const getWorkloadValuesUiSchema = (
+  user: GetSessionApiResponse['user'],
+  teamId: string,
+  valuesType: string,
+): any => {
+  const hidden = () => <div className='hiddenDiv' style={{ position: 'absolute' }} />
+  const advancedFields = ['env', 'args', 'labels', 'annotations', 'secrets', 'files', 'secretMounts', 'servicePorts']
+  const advancedUiSchema = advancedFields.reduce((acc: any, item: string) => {
+    acc[`${item}`] = {
+      'ui:widget': 'hidden',
+      'ui:field': hidden,
+      'ui:description': ' ',
+    }
+    return acc
+  }, {})
+
   const uiSchema = {
     id: { 'ui:widget': 'hidden' },
     teamId: { 'ui:widget': 'hidden' },
@@ -32,6 +59,12 @@ export const getWorkloadValuesUiSchema = (user: GetSessionApiResponse['user'], t
           removable: false,
         },
       },
+      resources: {
+        limits: valuesType !== 'Advanced' && { 'ui:widget': 'hidden' },
+      },
+      containerSecurityContext: valuesType !== 'Advanced' && { 'ui:widget': 'hidden' },
+      serviceMonitor: valuesType !== 'Advanced' && { 'ui:widget': 'hidden' },
+      ...(valuesType !== 'Advanced' && advancedUiSchema),
     },
   }
 
@@ -53,11 +86,11 @@ export default function ({ teamId, valuesData, setValuesData, selectedChart, ...
   const [valuesType, setValuesType] = useState('Basic')
   // END HOOKS
   const schema = getWorkloadValuesSchema(selectedChart, valuesType)
-  const uiSchema = getWorkloadValuesUiSchema(user, teamId)
+  const uiSchema = getWorkloadValuesUiSchema(user, teamId, valuesType)
 
   return (
-    <>
-      <FormControl sx={{ mt: 2 }}>
+    <Box sx={{ mt: '2rem' }}>
+      <FormControl>
         <RadioGroup
           sx={{ display: 'flex', flexDirection: 'row' }}
           onChange={(e) => setValuesType(e.target.value)}
@@ -79,6 +112,6 @@ export default function ({ teamId, valuesData, setValuesData, selectedChart, ...
         hideHelp
         {...other}
       />
-    </>
+    </Box>
   )
 }
