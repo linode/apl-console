af3c7ae57a9267c1643e80c0c0155fbdbc4d382b
Ferruh Cihan <ferruh.cihan@redkubes.com>
2023-08-14 20:48:51 +0200

    test: grafana

--- src/pages/Grafana.tsx
+++ src/pages/Grafana.tsx
@@ -1,53 +1,33 @@
 import PaperLayout from 'layouts/Paper'
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 export default function (): React.ReactElement {
-  const origin = window.location.origin
-  const [url, setUrl] = React.useState('')
-  const [iFrameUrl, setIFrameUrl] = React.useState(origin)
-
-  console.log('origin', origin)
+  const { search } = useLocation()
+  const params = new URLSearchParams(search)
+  const iframes = params?.get('iframes')?.split(',')
+  console.log('iframes', iframes)
 
   const comp = (
-    <div style={{ height: '80vh' }}>
+    <div>
       <h1>Grafana</h1>
-      <input type='text' onChange={(e) => setUrl(e.target.value)} />
-      <button type='button' onClick={() => setIFrameUrl(url)}>
-        Set URL
-      </button>
-      <iframe
-        title='Grafana iFrame'
-        src={iFrameUrl}
-        style={{
-          width: '100%',
-          height: '200px',
-          border: 'none',
-          marginTop: '10px',
-          marginBottom: '10px',
-        }}
-      />
-      <iframe
-        title='Grafana iFrame'
-        src={iFrameUrl}
-        style={{
-          width: '100%',
-          height: '200px',
-          border: 'none',
-          marginTop: '10px',
-          marginBottom: '10px',
-        }}
-      />
-      <iframe
-        title='Grafana iFrame'
-        src={iFrameUrl}
-        style={{
-          width: '100%',
-          height: '200px',
-          border: 'none',
-          marginTop: '10px',
-          marginBottom: '10px',
-        }}
-      />
+      {iframes ? (
+        iframes?.map((item) => (
+          <iframe
+            title='Grafana iFrame'
+            src={item}
+            style={{
+              width: '100%',
+              height: '200px',
+              border: 'none',
+              marginTop: '10px',
+              marginBottom: '10px',
+            }}
+          />
+        ))
+      ) : (
+        <div>...</div>
+      )}
     </div>
   )
   return <PaperLayout comp={comp} />
