b34b37ef98942d9cd0636271f151560dca2b2c93
Ferruh Cihan <ferruh.cihan@redkubes.com>
2023-08-15 09:29:59 +0200

    test: grafana

--- src/pages/Grafana.tsx
+++ src/pages/Grafana.tsx
@@ -5,7 +5,11 @@ import { useLocation } from 'react-router-dom'
 export default function (): React.ReactElement {
   const { search } = useLocation()
   const params = new URLSearchParams(search)
-  const iframes = params?.get('iframes')?.split(',')
+  // const iframes = params?.get('iframes')?.split(',')
+  const iframes = [
+    'https://grafana.51.158.129.24.nip.io/d-solo/iJiti6Lnkgg/kubernetes-cluster-status?orgId=1&refresh=30s&from=1692080796524&to=1692084396524&panelId=35',
+    'https://grafana.51.158.129.24.nip.io/d-solo/iJiti6Lnkgg/kubernetes-cluster-status?orgId=1&refresh=30s&from=1692080796524&to=1692084396524&panelId=34',
+  ]
   console.log('iframes', iframes)
 
   const comp = (
