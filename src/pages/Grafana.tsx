60cad51e1d88b18ab30461e09f3aabfa45263575
Ferruh Cihan <ferruh.cihan@redkubes.com>
2023-08-15 13:41:23 +0200

    test: grafana

--- src/pages/Grafana.tsx
+++ src/pages/Grafana.tsx
@@ -1,37 +1,70 @@
 import PaperLayout from 'layouts/Paper'
+import { getDomain } from 'layouts/Shell'
 import React from 'react'
-import { useLocation } from 'react-router-dom'
 
 export default function (): React.ReactElement {
-  const { search } = useLocation()
-  const params = new URLSearchParams(search)
-  // const iframes = params?.get('iframes')?.split(',')
-  const iframes = [
-    'https://grafana.51.158.129.24.nip.io/d-solo/iJiti6Lnkgg/kubernetes-cluster-status?orgId=1&refresh=30s&from=1692080796524&to=1692084396524&panelId=35',
-    'https://grafana.51.158.129.24.nip.io/d-solo/iJiti6Lnkgg/kubernetes-cluster-status?orgId=1&refresh=30s&panelId=34',
-  ]
-  console.log('iframes', iframes)
+  const hostname = window.location.hostname
+  const domain = getDomain(hostname)
+  const iFrameBaseLink = `https://grafana.${domain}/d-solo/iJiti6Lnkgg/kubernetes-cluster-status?orgId=1&refresh=30s&panelId=`
 
   const comp = (
     <div>
       <h1>Grafana</h1>
-      {iframes ? (
-        iframes?.map((item) => (
-          <iframe
-            title='Grafana iFrame'
-            src={item}
-            style={{
-              width: '100%',
-              height: '200px',
-              border: 'none',
-              marginTop: '10px',
-              marginBottom: '10px',
-            }}
-          />
-        ))
-      ) : (
-        <div>...</div>
-      )}
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'space-between',
+        }}
+      >
+        <iframe
+          title='Grafana iFrame'
+          src={`${iFrameBaseLink}12`}
+          style={{
+            width: '50%',
+            height: '200px',
+            border: 'none',
+            marginTop: '10px',
+            marginBottom: '10px',
+          }}
+        />
+        <iframe
+          title='Grafana iFrame'
+          src={`${iFrameBaseLink}13`}
+          style={{
+            width: '50%',
+            height: '200px',
+            border: 'none',
+            marginTop: '10px',
+            marginBottom: '10px',
+          }}
+        />
+      </div>
+      <div>
+        <iframe
+          title='Grafana iFrame'
+          src={`${iFrameBaseLink}35`}
+          style={{
+            width: '100%',
+            height: '200px',
+            border: 'none',
+            marginTop: '10px',
+            marginBottom: '10px',
+          }}
+        />
+        <iframe
+          title='Grafana iFrame'
+          src={`${iFrameBaseLink}34`}
+          style={{
+            width: '100%',
+            height: '200px',
+            border: 'none',
+            marginTop: '10px',
+            marginBottom: '10px',
+          }}
+        />
+      </div>
     </div>
   )
   return <PaperLayout comp={comp} />
