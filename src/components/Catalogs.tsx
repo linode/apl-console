b416b4c446facc5c52976cb326de0ab30445f9f3
Sander Rodenhuis <sander.rodenhuis@redkubes.com>
2023-12-13 08:17:33 +0100

    fix: change name

--- src/components/Catalogs.tsx
+++ src/components/Catalogs.tsx
@@ -25,16 +25,16 @@ const useStyles = makeStyles()((theme) => {
 
 const developerCatalogInfo = [
   {
-    title: 'What is the Developer Catalog?',
-    text: 'The Developer Catalog offers golden path templates for your projects. Choose the template to use, customize the values and submit to accelerate your developer workflow.',
+    title: 'What is the Catalog?',
+    text: 'The Catalog offers golden path templates for your projects. Choose the template to use, customize the values and submit to create a workload.',
   },
   {
     title: 'Who is maintaining the Catalog?',
-    text: 'The templates in the Developer Catalog are maintained by the platform administrator.',
+    text: 'The templates in the Catalog are maintained by the platform administrator.',
   },
   {
-    title: 'Why use the Developer Catalog?',
-    text: 'Whether you’re a beginner or an experienced developer, the catalog streamlines your workflow and makes deploying applications on Kubernetes a smooth and efficient proces.',
+    title: 'Why use the Catalog?',
+    text: 'The catalog streamlines your workflow and makes deploying workloads a smooth and efficient proces.',
   },
 ]
 
