79c9c272ae1f63aa1837272a009881c2f4cf4290
ferruh2021@gmail.com <ferruh.cihan@redkubes.com>
2023-12-13 08:05:58 +0100

    feat: update markdown overrides

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,19 +1,44 @@
-import { Card } from '@mui/material'
+import { Card, styled } from '@mui/material'
 import MarkdownJSX from 'markdown-to-jsx'
 
+// Higher-order component to generate markdown components
+const createMDComp = (Tag: any, style: any) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  const StyledTag = styled(Tag)({ ...style })
+  return function ({ children }: any) {
+    return <StyledTag>{children}</StyledTag>
+  }
+}
+
 // Markdown overrides -----------------------------------------------
-function Header1({ children }: any) {
-  return <h1 style={{ marginBottom: '16px', paddingBottom: '8px', borderBottom: '1px solid #6e768166' }}>{children}</h1>
-}
-function Header2({ children }: any) {
-  return <h2 style={{ marginBottom: '16px', paddingBottom: '8px', borderBottom: '1px solid #6e768166' }}>{children}</h2>
-}
-function Paragraph({ children }: any) {
-  return <p style={{ marginBottom: '16px', lineHeight: '24px' }}>{children}</p>
-}
-function Code({ children }: any) {
-  return <code style={{ borderRadius: '6px', padding: '1.5px 4px', backgroundColor: '#6e768166' }}>{children}</code>
-}
+const mb = { marginBottom: '16px' }
+const pb = { paddingBottom: '8px' }
+const lh = { lineHeight: '24px' }
+const border = { border: '1px solid #6e768164' }
+const borderBottom = { borderBottom: '1px solid #6e768164' }
+const headerStyle = { ...mb, ...pb, ...borderBottom }
+const tableCell = { padding: '6px 12px', ...border }
+
+const Header1 = createMDComp('h1', { ...headerStyle })
+const Header2 = createMDComp('h2', { ...headerStyle })
+const Header3 = createMDComp('h3', { ...mb })
+const Paragraph = createMDComp('p', { ...mb, ...lh })
+const Code = createMDComp('code', { borderRadius: '6px', padding: '1.5px 4px', backgroundColor: '#6e768164' })
+const Pre = createMDComp('pre', {
+  ...mb,
+  backgroundColor: '#6e768164',
+  padding: '12px',
+  borderRadius: '6px',
+  '& > code': {
+    backgroundColor: 'transparent',
+  },
+})
+const Ol = createMDComp('ol', { ...mb, paddingLeft: '32px' })
+const Li = createMDComp('li', { ...lh })
+const Table = createMDComp('table', { ...mb, borderCollapse: 'collapse' })
+const Tbody = createMDComp('tbody', { '& tr:nth-child(even)': { backgroundColor: '#6e768132' } })
+const Th = createMDComp('th', { ...tableCell })
+const Td = createMDComp('td', { ...tableCell })
 
 // interface and component -----------------------------------------------
 interface Props {
@@ -33,11 +58,35 @@ export default function Markdown({ readme, sx }: Props) {
             h2: {
               component: Header2,
             },
+            h3: {
+              component: Header3,
+            },
             p: {
               component: Paragraph,
             },
             code: {
               component: Code,
+            },
+            pre: {
+              component: Pre,
+            },
+            li: {
+              component: Li,
+            },
+            ol: {
+              component: Ol,
+            },
+            table: {
+              component: Table,
+            },
+            tbody: {
+              component: Tbody,
+            },
+            th: {
+              component: Th,
+            },
+            td: {
+              component: Td,
             },
           },
         }}
