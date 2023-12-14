fd2825f82fbcb8643d45d5ef9d95882707b0beae
ferruh2021@gmail.com <ferruh.cihan@redkubes.com>
2023-12-14 14:46:30 +0100

    test: service status

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,12 +1,15 @@
 /* eslint-disable no-nested-ternary */
 import { useSession } from 'providers/Session'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
-import { GetAllServicesApiResponse, GetTeamServicesApiResponse } from 'redux/otomiApi'
+import { GetAllServicesApiResponse, GetTeamServicesApiResponse, useStatusMutation } from 'redux/otomiApi'
+import { CircularProgress } from '@mui/material'
+import { useSocket, useSocketEvent } from 'socket.io-react-hook'
 import { HeadCell } from './EnhancedTable'
 import RLink from './Link'
 import ListTable from './ListTable'
 import MuiLink from './MuiLink'
+import Iconify from './Iconify'
 
 const getServiceLink = (isAdmin, ownerId): CallableFunction =>
   function (row): string | React.ReactElement {
@@ -32,6 +35,22 @@ const renderHost = ({ ingress, teamId, name }): React.ReactElement | string => {
   )
 }
 
+const getStatus = (row: any, statuses: any) => {
+  const status = statuses?.[row.name]
+  if (!status || status.status === 'NotFound') return <CircularProgress size='22px' />
+
+  switch (status.status) {
+    case 'Failed':
+      return <Iconify color='#FF4842' icon='eva:alert-circle-fill' width={22} height={22} />
+    // case 'OutOfSync':
+    //   return <Iconify color='#FFC107' icon='eva:alert-triangle-fill' width={22} height={22} />
+    case 'Ready':
+      return <Iconify color='#54D62C' icon='eva:checkmark-circle-2-fill' width={22} height={22} />
+    default:
+      return <CircularProgress size='22px' />
+  }
+}
+
 interface Props {
   services: GetAllServicesApiResponse | GetTeamServicesApiResponse
   teamId?: string
@@ -40,6 +59,22 @@ interface Props {
 
 // TODO: https://github.com/redkubes/otomi-core/discussions/475
 export default function ({ services, teamId, canCreateResource }: Props): React.ReactElement {
+  const url = `${window.location.origin.replace(/^http/, 'ws')}`
+  const path = '/api/ws'
+  const { socket } = useSocket({ url, path })
+  const { lastMessage } = useSocketEvent<any>(socket, 'services')
+  console.log('services:', lastMessage)
+  const [startStopStatus] = useStatusMutation()
+
+  useEffect(() => {
+    let intervalId: number
+    startStopStatus({ body: { resource: 'builds', operation: 'start' } }).then((res: any) => {
+      intervalId = res.data
+    })
+    return () => {
+      startStopStatus({ body: { resource: 'builds', operation: 'stop', intervalId } })
+    }
+  }, [])
   const {
     user: { isAdmin },
     oboTeamId,
@@ -62,6 +97,11 @@ export default function ({ services, teamId, canCreateResource }: Props): React.
       label: t('URL'),
       renderer: renderHost,
       component: MuiLink,
+    },
+    {
+      id: 'Status',
+      label: 'Status',
+      renderer: (row) => getStatus(row, lastMessage),
     },
   ]
   if (!teamId) {
