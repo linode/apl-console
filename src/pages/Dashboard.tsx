9967d46cdc563c8dbb2633626c978aec5558da33
ferruh2021@gmail.com <ferruh.cihan@redkubes.com>
2023-12-22 23:51:18 +0100

    feat: update dashboard page queries

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable no-prototype-builtins */
-import { skipToken } from '@reduxjs/toolkit/dist/query'
 import Dashboard from 'components/Dashboard'
 import useAuthzSession from 'hooks/useAuthzSession'
 import useSettings from 'hooks/useSettings'
@@ -8,18 +7,7 @@ import find from 'lodash/find'
 import React, { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useAppSelector } from 'redux/hooks'
-import {
-  useGetAllBuildsQuery,
-  useGetAllProjectsQuery,
-  useGetAllSecretsQuery,
-  useGetAllServicesQuery,
-  useGetAllWorkloadsQuery,
-  useGetTeamBuildsQuery,
-  useGetTeamProjectsQuery,
-  useGetTeamServicesQuery,
-  useGetTeamWorkloadsQuery,
-  useGetTeamsQuery,
-} from 'redux/otomiApi'
+import { useGetDashboardQuery, useGetTeamsQuery } from 'redux/otomiApi'
 
 export default function (): React.ReactElement {
   const { themeView } = useSettings()
@@ -36,86 +24,25 @@ export default function (): React.ReactElement {
   } = useGetTeamsQuery()
 
   const {
-    data: allProjects,
-    isFetching: isFetchingAllProjects,
-    refetch: refetchAllProjects,
-  } = useGetAllProjectsQuery(!isAdmin ? skipToken : undefined)
-  const {
-    data: teamProjects,
-    isFetching: isFetchingTeamProjects,
-    refetch: refetchTeamProjects,
-  } = useGetTeamProjectsQuery({ teamId })
-
-  const {
-    data: allBuilds,
-    isFetching: isFetchingAllBuilds,
-    refetch: refetchAllBuilds,
-  } = useGetAllBuildsQuery(!isAdmin ? skipToken : undefined)
-  const {
-    data: teamBuilds,
-    isFetching: isFetchingTeamBuilds,
-    refetch: refetchTeamBuilds,
-  } = useGetTeamBuildsQuery({ teamId })
-
-  const {
-    data: allWorkloads,
-    isFetching: isFetchingAllWorkloads,
-    refetch: refetchAllWorkloads,
-  } = useGetAllWorkloadsQuery(!isAdmin ? skipToken : undefined)
-  const {
-    data: teamWorkloads,
-    isFetching: isFetchingTeamWorkloads,
-    refetch: refetchTeamWorkloads,
-  } = useGetTeamWorkloadsQuery({ teamId })
-
-  const {
-    data: allServices,
-    isFetching: isFetchingAllServices,
-    refetch: refetchAllServices,
-  } = useGetAllServicesQuery(!isAdmin ? skipToken : undefined)
-  const {
-    data: teamServices,
-    isFetching: isFetchingTeamServices,
-    refetch: refetchTeamServices,
-  } = useGetTeamServicesQuery({ teamId })
-
-  const {
-    data: allSecrets,
-    isFetching: isFetchingAllSecrets,
-    refetch: refetchAllSecrets,
-  } = useGetAllSecretsQuery(!isAdmin ? skipToken : undefined)
-
-  const filteredSecrets = () => {
-    if (teamId === 'admin') return allSecrets
-    return allSecrets?.filter((secret: any) => secret.teamId === teamId)
-  }
+    data: dashboard,
+    isFetching: isFetchingDashboard,
+    refetch: refetchDashboard,
+  } = useGetDashboardQuery({ teamId })
 
   const isDirty = useAppSelector(({ global: { isDirty } }) => isDirty)
   useEffect(() => {
     if (isDirty !== false) return
-    if (!teamId && !isFetchingAllServices) refetchAllServices()
-    if (teamId && !isFetchingTeamServices) refetchTeamServices()
+    if (teamId && !isFetchingDashboard) refetchDashboard()
     if (teamId && !isFetchingTeams) refetchTeams()
   }, [isDirty])
   const { t } = useTranslation()
   // END HOOKS
   const team = !isLoadingTeams && find(teams, { id: teamId })
-  const loading = isFetchingAllServices || isFetchingTeamServices || isLoadingTeams
-  const projects = teamId === 'admin' ? allProjects : teamProjects
-  const builds = teamId === 'admin' ? allBuilds : teamBuilds
-  const workloads = teamId === 'admin' ? allWorkloads : teamWorkloads
-  const services = teamId === 'admin' ? allServices : teamServices
-  const secrets = filteredSecrets()
+  const loading = isFetchingDashboard || isLoadingTeams
   const teamInventory = themeView === 'platform' ? [{ name: 'teams', count: teams?.length }] : []
-  const inventory = [
-    ...teamInventory,
-    { name: 'projects', count: projects?.length },
-    { name: 'builds', count: builds?.length },
-    { name: 'workloads', count: workloads?.length },
-    { name: 'services', count: services?.length },
-    { name: 'secrets', count: secrets?.length },
-  ]
-  const comp = services && teams && <Dashboard team={team} inventory={inventory} />
+  const dashboardInventory = dashboard ?? ([] as any)
+  const inventory = [...teamInventory, ...dashboardInventory]
+  const comp = teams && dashboard && <Dashboard team={team} inventory={inventory} />
   return (
     <PaperLayout loading={loading} comp={comp} title={t('TITLE_DASHBOARD', { role: isAdmin ? 'admin' : 'team' })} />
   )
