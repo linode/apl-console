0e98bd1739043508ffed702dafaa106324ff665f
jeho <17126497+j-zimnowoda@users.noreply.github.com>
2024-02-13 17:20:21 +0100

    feat: download values (#388)
    * feat: get values
    
    * feat: add menu item
    
    * feat: add menu item
    
    * feat: get values

--- src/App.tsx
+++ src/App.tsx
@@ -49,6 +49,7 @@ import { SettingsProvider } from 'contexts/SettingsContext'
 import { getSettings } from 'utils/getSettings'
 import ThemeColorPresets from 'components/ThemeColorPresets'
 import Project from 'pages/Project'
+import Maintenance from 'pages/Maintenance'
 import { HttpErrorBadRequest } from './utils/error'
 import { NotistackProvider, SnackbarUtilsConfigurator } from './utils/snack'
 
@@ -124,6 +125,8 @@ function App() {
                                   <Route path='/teams/:teamId/workloads' component={Workloads} exact />
                                   <Route path='/teams/:teamId/services' component={Services} exact />
                                   <Route path='/teams/:teamId/services/:serviceId' component={Service} exact />
+                                  <Route path='/maintenance' component={Maintenance} exact />
+
                                   <Route path='*'>
                                     <Error error={new HttpErrorBadRequest()} />
                                   </Route>
