a268cf61c233c0a357158743c1bb6ea1a1f1fb4e
ferruhcihan <ferruh.cihan@redkubes.com>
2024-03-04 12:54:34 +0100

    fix: remove NavConfig duplicate items

--- src/components/NavConfig.tsx
+++ src/components/NavConfig.tsx
@@ -70,7 +70,6 @@ export default function NavConfig() {
         { title: 'Workloads', path: `/teams/${oboTeamId}/workloads/`, icon: getIcon('workloads_icon.svg') },
         { title: 'Services', path: `/teams/${oboTeamId}/services`, icon: getIcon('services_icon.svg') },
         { title: 'Security Policies', path: `/teams/${oboTeamId}/policies`, icon: getIcon('policies_icon.svg') },
-        { title: 'Secrets', path: `/teams/${oboTeamId}/secrets`, icon: getIcon('secrets_icon.svg') },
         {
           title: 'Settings',
           path: `/teams/${oboTeamId}`,
