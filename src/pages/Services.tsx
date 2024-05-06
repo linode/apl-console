e2448041c4d784adc868c06d9d0a18859bfcf5bd
Dennis van Kekem <dennis.vankekem@redkubes.com>
2024-05-06 13:58:18 +0200

    feat: removed license

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,14 +1,12 @@
 import { skipToken } from '@reduxjs/toolkit/dist/query'
 import Services from 'components/Services'
 import PaperLayout from 'layouts/Paper'
-import { useSession } from 'providers/Session'
 import React, { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { RouteComponentProps } from 'react-router-dom'
 import { useAppSelector } from 'redux/hooks'
-import { useGetAllServicesQuery, useGetMetricsQuery, useGetTeamServicesQuery } from 'redux/otomiApi'
+import { useGetAllServicesQuery, useGetTeamServicesQuery } from 'redux/otomiApi'
 import { getRole } from 'utils/data'
-import { canCreateAdditionalResource } from 'utils/permission'
 
 interface Params {
   teamId?: string
@@ -31,25 +29,17 @@ export default function ({
     isFetching: isFetchingTeamServices,
     refetch: refetchTeamServices,
   } = useGetTeamServicesQuery({ teamId }, { skip: !teamId })
-  const { data: metrics, isLoading: isLoadingMetrics } = useGetMetricsQuery()
   const isDirty = useAppSelector(({ global: { isDirty } }) => isDirty)
   useEffect(() => {
     if (isDirty !== false) return
     if (!teamId && !isFetchingAllServices) refetchAllServices()
     else if (teamId && !isFetchingTeamServices) refetchTeamServices()
   }, [isDirty])
-  const { license } = useSession()
   const { t } = useTranslation()
   // END HOOKS
 
-  const loading = isLoadingAllServices || isLoadingTeamServices || isLoadingMetrics
+  const loading = isLoadingAllServices || isLoadingTeamServices
   const services = teamId ? teamServices : allServices
-  const comp = services && (
-    <Services
-      services={services}
-      teamId={teamId}
-      canCreateResource={canCreateAdditionalResource('service', metrics, license)}
-    />
-  )
+  const comp = services && <Services services={services} teamId={teamId} />
   return <PaperLayout loading={loading} comp={comp} title={t('TITLE_SERVICES', { scope: getRole(teamId) })} />
 }
