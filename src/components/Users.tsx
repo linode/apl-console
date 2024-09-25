9be69b52ce19348f06e2a77a1487d3fc67a0e52b
Ferruh Cihan <63190600+ferruhcihan@users.noreply.github.com>
2024-09-25 11:28:29 +0200

    feat: update users table

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,10 +1,13 @@
 import { useSession } from 'providers/Session'
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { GetTeamUsersApiResponse } from 'redux/otomiApi'
-import { HeadCell } from './EnhancedTable'
-import RLink from './Link'
+import { Box, Tooltip } from '@mui/material'
+import ContentCopyIcon from '@mui/icons-material/ContentCopy'
+import DoneIcon from '@mui/icons-material/Done'
 import ListTable from './ListTable'
+import RLink from './Link'
+import { HeadCell } from './EnhancedTable'
 
 interface Row {
   teamId: string
@@ -24,6 +27,42 @@ const getUserLink = (row: Row) => {
   )
 }
 
+function CredentialsRenderer({ row, hostname }: { row: Row; hostname: string }) {
+  const [copied, setCopied] = useState(false)
+  const message = `
+  ###########################################################
+  You can start using APL. Visit: https://${hostname}
+  Sign in to the web console with the following credentials:
+    - Username: ${row.name}
+    - Password: ${row.name}@APL
+  You will be prompted to change your password after the first login.
+  ###########################################################
+  `
+
+  const handleCopyToClipboard = () => {
+    navigator.clipboard.writeText(message)
+    setCopied(true)
+    setTimeout(() => {
+      setCopied(false)
+    }, 3000)
+  }
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box sx={{ width: '30px' }}>
+        {!copied ? (
+          <Tooltip title='Copy initial credentials to clipboard'>
+            <ContentCopyIcon sx={{ ml: 1, cursor: 'pointer' }} onClick={handleCopyToClipboard} />
+          </Tooltip>
+        ) : (
+          <Tooltip title='Copied!'>
+            <DoneIcon sx={{ ml: 1, cursor: 'pointer' }} />
+          </Tooltip>
+        )}
+      </Box>
+    </Box>
+  )
+}
+
 interface Props {
   users: GetTeamUsersApiResponse
   teamId?: string
@@ -31,8 +70,8 @@ interface Props {
 
 export default function ({ users, teamId }: Props): React.ReactElement {
   const { user } = useSession()
-
   const { t } = useTranslation()
+  const hostname = window.location.hostname
   // END HOOKS
   const headCells: HeadCell[] = [
     {
@@ -44,6 +83,11 @@ export default function ({ users, teamId }: Props): React.ReactElement {
       id: 'email',
       label: t('Email'),
       renderer: (row) => row.email,
+    },
+    {
+      id: 'credentials',
+      label: t('Credentials'),
+      renderer: (row: Row) => <CredentialsRenderer row={row} hostname={hostname} />,
     },
   ]
 
