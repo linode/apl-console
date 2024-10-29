bf5bfa65b682198166212e8cde14f8ec85e3c1dd
Ferruh Cihan <63190600+ferruhcihan@users.noreply.github.com>
2024-10-30 00:16:50 +0100

    feat: update obj wizard

--- src/components/ObjectStorageWizard.tsx
+++ src/components/ObjectStorageWizard.tsx
@@ -1,4 +1,5 @@
-import { Box, Button, Modal, styled } from '@mui/material'
+import { Box, Button, Link, Modal, TextField, Typography, styled } from '@mui/material'
+import { useState } from 'react'
 import { useLocalStorage } from 'react-use'
 
 // styles ----------------------------------------------------------------
@@ -7,7 +8,7 @@ const ModalBox = styled(Box)(({ theme }) => ({
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  minWidth: 500,
+  width: 600,
   backgroundColor: theme.palette.background.paper,
   boxShadow:
     'rgb(0 0 0 / 20%) 0px 11px 15px -7px, rgb(0 0 0 / 14%) 0px 24px 38px 3px, rgb(0 0 0 / 12%) 0px 9px 46px 8px',
@@ -25,6 +26,7 @@ const ModalFooter = styled('div')({
   justifyContent: 'flex-end',
   padding: '20px',
   paddingRight: '30px',
+  gap: '10px',
 })
 
 // interface and component -----------------------------------------------
@@ -34,20 +36,71 @@ interface Props {
 
 export default function StyledModal({ open = true }: Props) {
   const [isWizardSkipped, setWizardSkipped] = useLocalStorage<string>('isWizardSkipped', JSON.stringify(false))
+  const [accepted, setAccepted] = useState(false)
+  const [apiToken, setApiToken] = useState('')
   const handleSkip = () => {
     setWizardSkipped(JSON.stringify(true))
+  }
+  const handleAccept = () => {
+    setAccepted(true)
+  }
+  const handleSubmit = () => {
+    console.log('API Token:', apiToken)
   }
   return (
     <Modal open={!JSON.parse(isWizardSkipped)}>
       <ModalBox>
         <ModalContent>
-          <Box>Wizard Content</Box>
+          {!accepted ? (
+            <Box>
+              <Typography variant='body1'>
+                The Application Platform requires Object Storage to support long term retention of logs, metrics and
+                traces, and storing backups and created container images.
+              </Typography>
+              <Typography variant='body1' mt={2}>
+                Would you like the Platform to create all the required buckets for you?
+              </Typography>
+            </Box>
+          ) : (
+            <Box>
+              <Typography variant='body1'>
+                The Application Platform needs an API Token to create the Object Storage.
+              </Typography>
+              <TextField
+                label='API Token'
+                fullWidth
+                onChange={(e) => {
+                  setApiToken(e.target.value)
+                }}
+              />
+              <Typography variant='body2' mt={2}>
+                <Link href='https://example.com/how-to-create-api-token' target='_blank' rel='noopener'>
+                  How to create an API token?
+                </Link>
+              </Typography>
+            </Box>
+          )}
         </ModalContent>
 
         <ModalFooter>
-          <Button variant='contained' color='primary' onClick={handleSkip}>
+          <Button variant='outlined' color='primary' onClick={handleSkip}>
             Skip
           </Button>
+
+          {!accepted ? (
+            <Button variant='contained' color='primary' onClick={handleAccept}>
+              Yes
+            </Button>
+          ) : (
+            <Box sx={{ display: 'flex', gap: '10px' }}>
+              <Button variant='outlined' color='primary' onClick={() => setAccepted(false)}>
+                Previous
+              </Button>
+              <Button variant='contained' color='primary' disabled={!apiToken} onClick={handleSubmit}>
+                Submit
+              </Button>
+            </Box>
+          )}
         </ModalFooter>
       </ModalBox>
     </Modal>
