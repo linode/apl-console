29e6771f3fb4996eaee04d503d5ed36ba1976200
Ferruh Cihan <63190600+ferruhcihan@users.noreply.github.com>
2024-11-13 10:10:56 +0100

    fix: clear errors before logout

--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -1,8 +1,12 @@
 import { useEffect } from 'react'
+import { useAppDispatch } from 'redux/hooks'
+import { setError } from 'redux/reducers'
 
 export default function Logout() {
   // This component will redirect the user to the Keycloak logout page
+  const dispatch = useAppDispatch()
   useEffect(() => {
+    dispatch(setError(undefined))
     window.location.href = '/logout-otomi'
   }, [])
   return null
