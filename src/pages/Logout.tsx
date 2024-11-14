c720cb9f818bc7cca8e33e5d62a4989a81679032
Ferruh Cihan <63190600+ferruhcihan@users.noreply.github.com>
2024-11-14 17:03:12 +0100

    test: logout

--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -1,7 +1,5 @@
 import LoadingScreen from 'components/LoadingScreen'
 import React, { useEffect } from 'react'
-import { useAppDispatch } from 'redux/hooks'
-import { setDirty, setError } from 'redux/reducers'
 
 interface Props {
   fetchError?: boolean
@@ -9,17 +7,12 @@ interface Props {
 
 export default function Logout({ fetchError = false }: Props): React.ReactElement {
   // This component will redirect the user to the Keycloak logout page
-  const dispatch = useAppDispatch()
   useEffect(() => {
-    dispatch(setDirty(false))
-    dispatch(setError(undefined))
     if (fetchError) {
       // automatically triggers Keycloak to route the user to the Keycloak login page
       window.location.reload()
     } else window.location.href = '/logout-otomi'
     return () => {
-      dispatch(setDirty(false))
-      dispatch(setError(undefined))
       window.location.reload()
     }
   }, [fetchError])
