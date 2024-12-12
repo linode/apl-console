7e02a54bae57a745fd71dfe6a5f599e439734710
Ferruh Cihan <63190600+ferruhcihan@users.noreply.github.com>
2024-12-12 16:50:35 +0100

    feat: add unit tests for product updates

--- src/components/UpgradeVersion.tsx
+++ src/components/UpgradeVersion.tsx
@@ -13,11 +13,12 @@ import {
 } from '@mui/material'
 import { KeyboardArrowRight, LocalOffer } from '@mui/icons-material'
 import axios from 'axios'
-import { findLast, isEmpty, takeRight } from 'lodash'
+import { findLast, isEmpty } from 'lodash'
 import { useSession } from 'providers/Session'
 import { useEditSettingsMutation, useGetSettingsQuery } from 'redux/otomiApi'
 import YAML from 'yaml'
 import Modal from './Modal'
+import { VersionInfo, parseUpdates } from '../utils/helpers'
 
 const StyledAccordion = styled(Accordion)(() => ({
   backgroundColor: 'transparent',
@@ -53,40 +54,6 @@ const StyledUpdateSection = styled(Box)(() => ({
 
 interface Props {
   version?: string
-}
-
-interface VersionUpdates {
-  currentVersionUpdates?: VersionInfo[]
-}
-
-interface VersionInfo {
-  version: string
-  message: string
-}
-
-function parseUpdates(updates: VersionInfo[], currentVersion: string): VersionUpdates {
-  const parseVersion = (version: string) => version.replace('v', '').split('.').map(Number)
-
-  const compareVersions = (v1: string, v2: string) => {
-    const [major1, minor1, patch1] = parseVersion(v1)
-    const [major2, minor2, patch2] = parseVersion(v2)
-    if (major1 !== major2) return major1 - major2
-    if (minor1 !== minor2) return minor1 - minor2
-    return patch1 - patch2
-  }
-
-  const [currentMajor] = parseVersion(currentVersion)
-
-  const currentVersionUpdates = updates
-    .filter(({ version }) => {
-      const [major] = parseVersion(version)
-      return major === currentMajor && compareVersions(version, currentVersion) > 0
-    })
-    .sort((a, b) => compareVersions(a.version, b.version))
-
-  return {
-    currentVersionUpdates: takeRight(currentVersionUpdates, 5),
-  }
 }
 
 export default function UpgradesCard({ version }: Props): React.ReactElement | null {
