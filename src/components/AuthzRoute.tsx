27b82e3da302e2e930d49735e605420d344d3bd5
Ferruh <63190600+ferruhcihan@users.noreply.github.com>
2024-12-17 10:23:13 +0100

    fix: authz route apps page access (#487)

--- src/components/AuthzRoute.tsx
+++ src/components/AuthzRoute.tsx
@@ -3,14 +3,16 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import Error from 'pages/Error'
 import { HttpErrorForbidden } from 'utils/error'
+import useSettings from 'hooks/useSettings'
 import NavConfig from './NavConfig'
 
 function PrivateRoute({ component: Component, platformAdminRoute, teamAdminRoute, ...rest }: any) {
+  const { themeView } = useSettings()
   const navs = NavConfig()
   let isAbleToAccess = true
   navs.forEach((group) => {
     group.items.forEach((item) => {
-      if (item.path === rest.location.pathname && item.hidden) isAbleToAccess = false
+      if (item.path === rest.location.pathname && item.hidden && themeView === 'team') isAbleToAccess = false
     })
   })
   const session = useSession()
