4999b33f8b1d5129f546cd5083f4bcd51b6f34ba
Ferruh Cihan <63190600+ferruhcihan@users.noreply.github.com>
2025-02-04 21:21:34 +0100

    feat: remove react-dnd

--- src/App.tsx
+++ src/App.tsx
@@ -29,8 +29,6 @@ import Policies from 'pages/Policies'
 import SessionProvider from 'providers/Session'
 import ThemeProvider from 'theme'
 import React, { Suspense } from 'react'
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
 import { ErrorBoundary } from 'react-error-boundary'
 import Helmet from 'react-helmet'
 import { Provider } from 'react-redux'
@@ -79,114 +77,102 @@ function App() {
               <Router basename={contextPath}>
                 <ErrorBoundary FallbackComponent={Error}>
                   <CacheProvider value={muiCache}>
-                    <DndProvider backend={HTML5Backend}>
-                      <IoProvider>
-                        <SessionProvider>
-                          <ShellDrawerProvider>
-                            <CollapseDrawerProvider>
-                              <NotistackProvider>
-                                <SnackbarUtilsConfigurator />
-                                <CssBaseline />
-                                {/* <ProgressBar /> */}
-                                <Helmet titleTemplate='%s | APL' defaultTitle='Akamai APL Platform' />
-                                <Switch>
-                                  {/* ! user && <Route path='/' component={Home} exact /> */}
-                                  <Route path='/' component={Dashboard} exact />
-                                  <PrivateRoute path='/apps/:teamId' component={Apps} exact />
-                                  <PrivateRoute path='/apps/:teamId/:appId' component={OtomiApp} exact />
-                                  <PrivateRoute path='/backups' component={Backups} platformAdminRoute exact />
-                                  <PrivateRoute
-                                    path='/clusters/:clusterId'
-                                    component={Cluster}
-                                    platformAdminRoute
-                                    exact
-                                  />
-                                  <PrivateRoute path='/clusters' component={Clusters} platformAdminRoute exact />
-                                  <PrivateRoute path='/create-team' component={Team} platformAdminRoute exact />
-                                  <PrivateRoute path='/netpols' component={Netpols} platformAdminRoute exact />
-                                  <PrivateRoute path='/policies' component={Policies} platformAdminRoute exact />
-                                  <PrivateRoute
-                                    path='/policies/:policyId'
-                                    component={Policy}
-                                    platformAdminRoute
-                                    exact
-                                  />
-                                  <PrivateRoute path='/catalogs/:teamId' component={Catalogs} exact />
-                                  <PrivateRoute path='/catalogs/:teamId/:catalogName' component={Catalog} exact />
-                                  <PrivateRoute
-                                    path='/catalogs/:teamId/:catalogName/:workloadId'
-                                    component={Catalog}
-                                    exact
-                                  />
-                                  <PrivateRoute path='/services' component={Services} platformAdminRoute exact />
-                                  <PrivateRoute path='/secrets' component={Secrets} platformAdminRoute exact />
-                                  <PrivateRoute
-                                    path='/sealed-secrets'
-                                    component={SealedSecrets}
-                                    platformAdminRoute
-                                    exact
-                                  />
-                                  <PrivateRoute path='/workloads' component={Workloads} platformAdminRoute exact />
-                                  <PrivateRoute
-                                    path='/settings'
-                                    component={SettingsOverview}
-                                    platformAdminRoute
-                                    exact
-                                  />
-                                  <PrivateRoute path='/users' component={Users} platformAdminRoute exact />
-                                  <PrivateRoute path='/users/:userId' component={User} platformAdminRoute exact />
-                                  <PrivateRoute path='/teams/:teamId/users' component={Users} teamAdminRoute exact />
-                                  <PrivateRoute path='/projects' component={Projects} platformAdminRoute exact />
-                                  <PrivateRoute path='/builds' component={Builds} platformAdminRoute exact />
-                                  <PrivateRoute path='/settings/:settingId' component={Setting} exact />
-                                  <PrivateRoute path='/teams' component={Teams} platformAdminRoute exact />
-                                  <PrivateRoute path='/teams/:teamId' component={Team} exact />
-                                  <PrivateRoute path='/teams/:teamId/create-backup' component={Backup} exact />
-                                  <PrivateRoute path='/teams/:teamId/create-netpol' component={Netpol} exact />
-                                  <PrivateRoute path='/teams/:teamId/create-secret' component={Secret} exact />
-                                  <PrivateRoute
-                                    path='/teams/:teamId/create-sealedsecret'
-                                    component={SealedSecret}
-                                    exact
-                                  />
-                                  <PrivateRoute path='/teams/:teamId/create-service' component={Service} exact />
-                                  <PrivateRoute path='/teams/:teamId/create-user' component={User} exact />
-                                  <PrivateRoute path='/teams/:teamId/create-project' component={Project} exact />
-                                  <PrivateRoute path='/teams/:teamId/create-build' component={Build} exact />
-                                  <PrivateRoute path='/teams/:teamId/secrets' component={Secrets} exact />
-                                  <PrivateRoute path='/teams/:teamId/sealed-secrets' component={SealedSecrets} exact />
-                                  <PrivateRoute path='/teams/:teamId/secrets/:secretId' component={Secret} exact />
-                                  <PrivateRoute
-                                    path='/teams/:teamId/sealed-secrets/:secretId'
-                                    component={SealedSecret}
-                                    exact
-                                  />
-                                  <PrivateRoute path='/teams/:teamId/backups' component={Backups} exact />
-                                  <PrivateRoute path='/teams/:teamId/backups/:backupId' component={Backup} exact />
-                                  <PrivateRoute path='/teams/:teamId/netpols' component={Netpols} exact />
-                                  <PrivateRoute path='/teams/:teamId/netpols/:netpolId' component={Netpol} exact />
-                                  <PrivateRoute path='/teams/:teamId/projects' component={Projects} exact />
-                                  <PrivateRoute path='/teams/:teamId/projects/:projectId' component={Project} exact />
-                                  {/* <Route path='/teams/:teamId/builds' component={Builds} exact /> */}
-                                  <PrivateRoute exact path='/teams/:teamId/builds' component={Builds} />
-                                  <PrivateRoute path='/teams/:teamId/builds/:buildId' component={Build} exact />
-                                  <PrivateRoute path='/teams/:teamId/policies' component={Policies} exact />
-                                  <PrivateRoute path='/teams/:teamId/policies/:policyId' component={Policy} exact />
-                                  <PrivateRoute path='/teams/:teamId/workloads' component={Workloads} exact />
-                                  <PrivateRoute path='/teams/:teamId/services' component={Services} exact />
-                                  <PrivateRoute path='/teams/:teamId/services/:serviceId' component={Service} exact />
-                                  <PrivateRoute path='/maintenance' component={Maintenance} platformAdminRoute exact />
-                                  <Route path='/logout' component={Logout} />
-                                  <Route path='*'>
-                                    <Error error={new HttpErrorBadRequest()} />
-                                  </Route>
-                                </Switch>
-                              </NotistackProvider>
-                            </CollapseDrawerProvider>
-                          </ShellDrawerProvider>
-                        </SessionProvider>
-                      </IoProvider>
-                    </DndProvider>
+                    <IoProvider>
+                      <SessionProvider>
+                        <ShellDrawerProvider>
+                          <CollapseDrawerProvider>
+                            <NotistackProvider>
+                              <SnackbarUtilsConfigurator />
+                              <CssBaseline />
+                              {/* <ProgressBar /> */}
+                              <Helmet titleTemplate='%s | APL' defaultTitle='Akamai APL Platform' />
+                              <Switch>
+                                {/* ! user && <Route path='/' component={Home} exact /> */}
+                                <Route path='/' component={Dashboard} exact />
+                                <PrivateRoute path='/apps/:teamId' component={Apps} exact />
+                                <PrivateRoute path='/apps/:teamId/:appId' component={OtomiApp} exact />
+                                <PrivateRoute path='/backups' component={Backups} platformAdminRoute exact />
+                                <PrivateRoute
+                                  path='/clusters/:clusterId'
+                                  component={Cluster}
+                                  platformAdminRoute
+                                  exact
+                                />
+                                <PrivateRoute path='/clusters' component={Clusters} platformAdminRoute exact />
+                                <PrivateRoute path='/create-team' component={Team} platformAdminRoute exact />
+                                <PrivateRoute path='/netpols' component={Netpols} platformAdminRoute exact />
+                                <PrivateRoute path='/policies' component={Policies} platformAdminRoute exact />
+                                <PrivateRoute path='/policies/:policyId' component={Policy} platformAdminRoute exact />
+                                <PrivateRoute path='/catalogs/:teamId' component={Catalogs} exact />
+                                <PrivateRoute path='/catalogs/:teamId/:catalogName' component={Catalog} exact />
+                                <PrivateRoute
+                                  path='/catalogs/:teamId/:catalogName/:workloadId'
+                                  component={Catalog}
+                                  exact
+                                />
+                                <PrivateRoute path='/services' component={Services} platformAdminRoute exact />
+                                <PrivateRoute path='/secrets' component={Secrets} platformAdminRoute exact />
+                                <PrivateRoute
+                                  path='/sealed-secrets'
+                                  component={SealedSecrets}
+                                  platformAdminRoute
+                                  exact
+                                />
+                                <PrivateRoute path='/workloads' component={Workloads} platformAdminRoute exact />
+                                <PrivateRoute path='/settings' component={SettingsOverview} platformAdminRoute exact />
+                                <PrivateRoute path='/users' component={Users} platformAdminRoute exact />
+                                <PrivateRoute path='/users/:userId' component={User} platformAdminRoute exact />
+                                <PrivateRoute path='/teams/:teamId/users' component={Users} teamAdminRoute exact />
+                                <PrivateRoute path='/projects' component={Projects} platformAdminRoute exact />
+                                <PrivateRoute path='/builds' component={Builds} platformAdminRoute exact />
+                                <PrivateRoute path='/settings/:settingId' component={Setting} exact />
+                                <PrivateRoute path='/teams' component={Teams} platformAdminRoute exact />
+                                <PrivateRoute path='/teams/:teamId' component={Team} exact />
+                                <PrivateRoute path='/teams/:teamId/create-backup' component={Backup} exact />
+                                <PrivateRoute path='/teams/:teamId/create-netpol' component={Netpol} exact />
+                                <PrivateRoute path='/teams/:teamId/create-secret' component={Secret} exact />
+                                <PrivateRoute
+                                  path='/teams/:teamId/create-sealedsecret'
+                                  component={SealedSecret}
+                                  exact
+                                />
+                                <PrivateRoute path='/teams/:teamId/create-service' component={Service} exact />
+                                <PrivateRoute path='/teams/:teamId/create-user' component={User} exact />
+                                <PrivateRoute path='/teams/:teamId/create-project' component={Project} exact />
+                                <PrivateRoute path='/teams/:teamId/create-build' component={Build} exact />
+                                <PrivateRoute path='/teams/:teamId/secrets' component={Secrets} exact />
+                                <PrivateRoute path='/teams/:teamId/sealed-secrets' component={SealedSecrets} exact />
+                                <PrivateRoute path='/teams/:teamId/secrets/:secretId' component={Secret} exact />
+                                <PrivateRoute
+                                  path='/teams/:teamId/sealed-secrets/:secretId'
+                                  component={SealedSecret}
+                                  exact
+                                />
+                                <PrivateRoute path='/teams/:teamId/backups' component={Backups} exact />
+                                <PrivateRoute path='/teams/:teamId/backups/:backupId' component={Backup} exact />
+                                <PrivateRoute path='/teams/:teamId/netpols' component={Netpols} exact />
+                                <PrivateRoute path='/teams/:teamId/netpols/:netpolId' component={Netpol} exact />
+                                <PrivateRoute path='/teams/:teamId/projects' component={Projects} exact />
+                                <PrivateRoute path='/teams/:teamId/projects/:projectId' component={Project} exact />
+                                {/* <Route path='/teams/:teamId/builds' component={Builds} exact /> */}
+                                <PrivateRoute exact path='/teams/:teamId/builds' component={Builds} />
+                                <PrivateRoute path='/teams/:teamId/builds/:buildId' component={Build} exact />
+                                <PrivateRoute path='/teams/:teamId/policies' component={Policies} exact />
+                                <PrivateRoute path='/teams/:teamId/policies/:policyId' component={Policy} exact />
+                                <PrivateRoute path='/teams/:teamId/workloads' component={Workloads} exact />
+                                <PrivateRoute path='/teams/:teamId/services' component={Services} exact />
+                                <PrivateRoute path='/teams/:teamId/services/:serviceId' component={Service} exact />
+                                <PrivateRoute path='/maintenance' component={Maintenance} platformAdminRoute exact />
+                                <Route path='/logout' component={Logout} />
+                                <Route path='*'>
+                                  <Error error={new HttpErrorBadRequest()} />
+                                </Route>
+                              </Switch>
+                            </NotistackProvider>
+                          </CollapseDrawerProvider>
+                        </ShellDrawerProvider>
+                      </SessionProvider>
+                    </IoProvider>
                   </CacheProvider>
                 </ErrorBoundary>
               </Router>
