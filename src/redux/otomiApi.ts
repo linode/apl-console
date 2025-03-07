11f58000a650ae897fb6fe4be07b8bf3d630a4a8
Dennis van Kekem <38350840+dennisvankekem@users.noreply.github.com>
2025-03-07 09:58:12 +0100

    feat: APL-556, Add self-service option for admins to add external helm charts to the catalog (#519)
    * feat: add chart modal primer
    
    * feat: added createWorkloadCatalog endpoint
    
    * feat: add chart
    
    * feat: update workload ui schema
    
    * feat: added icon picker
    
    * feat: allow teams to use this chart checkbox
    
    * test: close new chart modal
    
    * fix: minor ux improvements
    
    * fix: only enable fields when test connction button is pressed
    
    * fix: add chart button state
    
    * fix: additional url validation
    
    * fix: added loading state for button, removed logs
    
    * fix: cleanup
    
    * fix: button responsiveness
    
    * fix: changed test connection button name
    
    * fix: changed icon location
    
    * feat: newChartModal test
    
    ---------
    
    Co-authored-by: Ferruh Cihan <63190600+ferruhcihan@users.noreply.github.com>

--- src/redux/otomiApi.ts
+++ src/redux/otomiApi.ts
@@ -263,6 +263,9 @@ const injectedRtkApi = api.injectEndpoints({
     workloadCatalog: build.mutation<WorkloadCatalogApiResponse, WorkloadCatalogApiArg>({
       query: (queryArg) => ({ url: `/workloadCatalog`, method: 'POST', body: queryArg.body }),
     }),
+    createWorkloadCatalog: build.mutation<CreateWorkloadCatalogApiResponse, CreateWorkloadCatalogApiArg>({
+      query: (queryArg) => ({ url: `/createWorkloadCatalog`, method: 'POST', body: queryArg.body }),
+    }),
     getTeamWorkloads: build.query<GetTeamWorkloadsApiResponse, GetTeamWorkloadsApiArg>({
       query: (queryArg) => ({ url: `/teams/${queryArg.teamId}/workloads` }),
     }),
@@ -3478,7 +3481,12 @@ export type GetAllWorkloadsApiResponse = /** status 200 Successfully obtained al
 }[]
 export type GetAllWorkloadsApiArg = void
 export type WorkloadCatalogApiResponse = /** status 200 Successfully updated a team project */ object
+export type CreateWorkloadCatalogApiResponse = /** status 200 Successfully updated a team project */ object
 export type WorkloadCatalogApiArg = {
+  /** Project object that contains updated values */
+  body: object
+}
+export type CreateWorkloadCatalogApiArg = {
   /** Project object that contains updated values */
   body: object
 }
@@ -4537,6 +4545,7 @@ export const {
   useDeleteCoderepoMutation,
   useGetAllWorkloadsQuery,
   useWorkloadCatalogMutation,
+  useCreateWorkloadCatalogMutation,
   useGetTeamWorkloadsQuery,
   useCreateWorkloadMutation,
   useDeleteWorkloadMutation,
