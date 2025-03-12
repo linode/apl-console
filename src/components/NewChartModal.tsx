d33baac0a222cd6f92ea051e89e8c550e721b4f3
Ferruh Cihan <63190600+ferruhcihan@users.noreply.github.com>
2025-03-12 15:09:26 +0100

    feat: hide new chart modal scrollbar

--- src/components/NewChartModal.tsx
+++ src/components/NewChartModal.tsx
@@ -32,6 +32,12 @@ const ModalBox = styled(Box)(({ theme }) => ({
     'rgb(0 0 0 / 20%) 0px 11px 15px -7px, rgb(0 0 0 / 14%) 0px 24px 38px 3px, rgb(0 0 0 / 12%) 0px 9px 46px 8px',
   borderRadius: 16,
   padding: 0,
+  // Hide scrollbar
+  '-ms-overflow-style': 'none' /* Internet Explorer 10+ */,
+  'scrollbar-width': 'none' /* Firefox */,
+  '&::-webkit-scrollbar': {
+    display: 'none' /* Safari and Chrome */,
+  },
 }))
 
 const ModalHeader = styled('div')({
