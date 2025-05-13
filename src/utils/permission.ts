911341c4e4622c01bedcf32642ec3443bd4a14a5
Ferruh <63190600+ferruhcihan@users.noreply.github.com>
2025-05-13 10:22:30 +0200

    fix: navconfig permission function (#555)

--- src/utils/permission.ts
+++ src/utils/permission.ts
@@ -4,6 +4,6 @@ import { GetSessionApiResponse } from 'redux/otomiApi'
 export function canDo(user: GetSessionApiResponse['user'], teamId: string, action: string): boolean {
   if (user.isPlatformAdmin) return true
   if (!teamId) return false
-  const deniedActions = get(user, `authz.${teamId}.deniedAttributes.access`, [])
+  const deniedActions = get(user, `authz.${teamId}.deniedAttributes.teamMembers`, [])
   return !deniedActions.includes(action)
 }
