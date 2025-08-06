7d2029e13f1b8ec8556d7a58c89ff8219007d190
dvankeke <dvankeke@akamai.com>
2025-08-06 16:02:24 +0200

    fix: removed comments

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -2,14 +2,12 @@ import React from 'react'
 import { styled } from '@mui/material/styles'
 import { Typography, TypographyProps } from './Typography'
 
-// styled part, separated
 const StyledDescription = styled(Typography)<TypographyProps>({
   color: '#ABABAB',
   marginTop: '5px',
   maxWidth: '85%',
 })
 
-// wrapper component
 export function Description(props: TypographyProps) {
   return <StyledDescription {...props} />
 }
