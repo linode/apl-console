b7bb6bf30dd6571ba109f2e1bf08e76dceec01a6
dvankeke <dvankeke@akamai.com>
2025-10-01 14:13:57 +0200

    feat: code editor with comments preserved

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -20,7 +20,7 @@ import {
 import YAML from 'yaml'
 import { cloneDeep, omit } from 'lodash'
 import { CrudProps } from 'pages/types'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { GetSessionApiResponse } from 'redux/otomiApi'
 import { useTranslation } from 'react-i18next'
 import { useHistory, useLocation } from 'react-router-dom'
@@ -36,7 +36,7 @@ import DeleteButton from './DeleteButton'
 import TabPanel from './TabPanel'
 import InformationBanner from './InformationBanner'
 import Iconify from './Iconify'
-import CodeEditor from './CodeEditor'
+import CodeEditor from '../pages/workloads/create-edit/WorkloadsCodeEditor'
 
 const useStyles = makeStyles()((theme) => ({
   header: {
@@ -56,7 +56,6 @@ const useStyles = makeStyles()((theme) => ({
     paddingTop: theme.spacing(1),
     paddingRight: theme.spacing(2),
     display: 'inline-flex',
-    // width: theme.spacing(8),
   },
   img: {
     height: theme.spacing(6),
@@ -136,25 +135,31 @@ export default function ({
   const dispatch = useAppDispatch()
   const { user } = useSession()
   const hash = location.hash.substring(1)
-  const hashMap = {
-    info: 0,
-    values: 1,
-  }
+  const hashMap = { info: 0, values: 1 }
   const defTab = hashMap[hash] ?? hashMap.info
   const [tab, setTab] = useState(defTab)
-  const handleTabChange = (event, tab) => {
-    // on the values tab, reset the values to see the comments in the code editor
-    if (tab === 1 && !workloadName) setWorkloadValues(values)
-    setTab(tab)
-  }
+  const handleTabChange = (event, tab) => setTab(tab)
+
   const [data, setData] = useState<any>(workload)
-  const [workloadValues, setWorkloadValues] = useState<any>(values)
+
+  const [workloadValuesYaml, setWorkloadValuesYaml] = useState(
+    typeof values === 'string' ? values : YAML.stringify(values ?? {}),
+  )
+
+  const workloadValuesJson = useMemo(() => {
+    try {
+      return YAML.parse(workloadValuesYaml)
+    } catch {
+      return {}
+    }
+  }, [workloadValuesYaml])
+
   const icon = data?.icon || '/logos/akamai_logo.svg'
 
   useEffect(() => {
     if (!workload) return
-    setWorkloadValues(values)
     setData(workload)
+    setWorkloadValuesYaml(typeof values === 'string' ? values : YAML.stringify(values ?? {}))
   }, [workload, values])
 
   const handleCreateUpdateWorkload = async () => {
@@ -182,7 +187,6 @@ export default function ({
       window.scrollTo({ top: 0, behavior: 'smooth' })
       return
     }
-
     const workloadBody = omit(data, ['chartProvider', 'chart', 'revision'])
     const chartMetadata = omit(data?.chartMetadata, ['helmChartCatalog', 'helmChart'])
     const path = workload?.path
@@ -191,25 +195,24 @@ export default function ({
       kind: 'AplTeamWorkload',
       metadata: {
         name: workloadName ?? data?.name,
-        labels: {
-          'apl.io/teamId': teamId,
-        },
+        labels: { 'apl.io/teamId': teamId },
       },
       spec: {
         ...workloadBody,
         chartMetadata,
         url: workload?.url,
         path,
-        values: YAML.stringify(workloadValues),
+        values: workloadValuesYaml,
       },
     }
+
     let res
     if (workloadName) {
       dispatch(setError(undefined))
       res = await updateWorkload({ teamId, workloadName, body })
     } else res = await createWorkload({ teamId, body })
-    if (res.error) return
-    history.push(`/teams/${teamId}/workloads`)
+
+    if (!res.error) history.push(`/teams/${teamId}/workloads`)
   }
 
   const schema = getWorkloadSchema()
@@ -223,6 +226,7 @@ export default function ({
             className={classes.img}
             src={icon}
             onError={({ currentTarget }) => {
+              // might need another fix
               // eslint-disable-next-line no-param-reassign
               currentTarget.onerror = null // prevents looping
               // eslint-disable-next-line no-param-reassign
@@ -232,15 +236,11 @@ export default function ({
           />
         </Box>
         <Box>
-          <Typography variant='h6'>{workload?.name ? `${workload.name} (${workload.path})` : workload.path}</Typography>
+          <Typography variant='h6'>
+            {workload?.name ? `${workload.name} (${workload.path})` : workload?.path}
+          </Typography>
         </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            ml: 'auto',
-          }}
-        >
+        <Box sx={{ display: 'flex', alignItems: 'center', ml: 'auto' }}>
           <ButtonGroup>
             {tab === 1 && (
               <Button variant='contained' onClick={handleCreateUpdateWorkload}>
@@ -261,17 +261,6 @@ export default function ({
 
       <AppBar position='relative' color='default'>
         <Tabs value={tab} onChange={handleTabChange} sx={{ ml: 1 }}>
-          {tab !== 1 && (
-            <Box
-              sx={{
-                position: 'absolute',
-                width: '45px',
-                left: '90px',
-                bottom: '0px',
-                animation: 'pulse 2s infinite',
-              }}
-            />
-          )}
           <Tab href='#info' label='Info' value={hashMap.info} />
           <Tab href='#values' label={t('Values')} value={hashMap.values} />
         </Tabs>
@@ -282,7 +271,7 @@ export default function ({
           <Grid item xs={12} md={6}>
             <Box className={classes.content}>
               <TableContainer className={classes.legend}>
-                <Table size='small' aria-label='simple table'>
+                <Table size='small'>
                   <TableBody>
                     <TableRow key='version' className={classes.tableRow}>
                       <TableCell component='th' scope='row' align='right'>
@@ -290,14 +279,12 @@ export default function ({
                       </TableCell>
                       <TableCell align='left'>{workload?.chartMetadata?.helmChartVersion}</TableCell>
                     </TableRow>
-
                     <TableRow key='description' className={classes.tableRow}>
                       <TableCell component='th' scope='row' align='right'>
                         <Chip label={t('Description:')} />
                       </TableCell>
                       <TableCell align='left'>{workload?.chartMetadata?.helmChartDescription}</TableCell>
                     </TableRow>
-
                     <TableRow key='repo' className={classes.tableRow}>
                       <TableCell component='th' scope='row' align='right'>
                         <Chip label={t('Repo:')} />
@@ -322,9 +309,10 @@ export default function ({
       </TabPanel>
 
       <TabPanel value={tab} index={hashMap.values}>
-        {workloadValues?.image && checkImageFields(workloadValues?.image) && (
-          <InformationBanner message={checkImageFields(workloadValues?.image)} />
+        {workloadValuesJson?.image && checkImageFields(workloadValuesJson?.image) && (
+          <InformationBanner message={checkImageFields(workloadValuesJson?.image)} />
         )}
+
         <Form
           schema={schema}
           uiSchema={uiSchema}
@@ -353,21 +341,9 @@ export default function ({
           </Tooltip>
         )}
 
-        <CodeEditor
-          code={workloadValues}
-          onChange={(data) => {
-            setWorkloadValues(data)
-          }}
-          setWorkloadValues={setWorkloadValues}
-        />
-        <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            ml: 'auto',
-            float: 'right',
-          }}
-        >
+        <CodeEditor code={workloadValuesYaml} onChange={setWorkloadValuesYaml} />
+
+        <Box sx={{ display: 'flex', alignItems: 'center', ml: 'auto', float: 'right' }}>
           <ButtonGroup>
             {tab === 1 && (
               <Button variant='contained' onClick={handleCreateUpdateWorkload}>
