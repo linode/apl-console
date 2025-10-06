832fe0c628483843fc9531b51ec5c6d664965d69
Ferruh Cihan <63190600+ferruhcihan@users.noreply.github.com>
2025-10-06 15:26:44 +0200

    test: agent playground

--- nginx/run.sh
+++ nginx/run.sh
@@ -2,11 +2,9 @@
 
 CONTEXT_PATH=${CONTEXT_PATH:-''}
 
-# Ensure KUBE_DNS_SERVICE_HOST is available for envsubst
-export KUBE_DNS_SERVICE_HOST
-
 find build -type f -print0 | xargs -0 sed -i -e "s/##CONTEXT_PATH##/$CONTEXT_PATH/g"
 
-envsubst "$(env | sed -e 's/=.*//' -e 's/^/\$/g')" </app/nginx.tmpl >/etc/nginx/nginx.conf
+# Generate nginx.conf with proper DNS resolver
+sed "s/\${KUBE_DNS_SERVICE_HOST}/$KUBE_DNS_SERVICE_HOST/g" /app/nginx.tmpl > /etc/nginx/nginx.conf
 
 exec nginx -g 'daemon off;'
