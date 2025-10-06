f9d73881c80d7643e178efda4662cd2ff7907aed
Ferruh Cihan <63190600+ferruhcihan@users.noreply.github.com>
2025-10-06 16:25:20 +0200

    test: agent playground

--- nginx/run.sh
+++ nginx/run.sh
@@ -4,10 +4,10 @@ CONTEXT_PATH=${CONTEXT_PATH:-''}
 
 find build -type f -print0 | xargs -0 sed -i -e "s/##CONTEXT_PATH##/$CONTEXT_PATH/g"
 
-# Get DNS nameserver from /etc/resolv.conf or use KUBE_DNS_SERVICE_HOST if available
-DNS_SERVER=${KUBE_DNS_SERVICE_HOST:-$(grep -m1 '^nameserver' /etc/resolv.conf | awk '{print $2}')}
+# Extract DNS nameserver for nginx resolver (fallback to resolv.conf if env var not set)
+DNS_SERVER=${KUBE_DNS_SERVICE_HOST:-$(awk '/^nameserver/{print $2; exit}' /etc/resolv.conf)}
 
-# Generate nginx.conf with proper DNS resolver
+# Generate nginx.conf with DNS resolver substituted
 sed "s/\${KUBE_DNS_SERVICE_HOST}/$DNS_SERVER/g" /app/nginx.tmpl > /etc/nginx/nginx.conf
 
 exec nginx -g 'daemon off;'
