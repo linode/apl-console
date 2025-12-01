3caec8c10e013ac08843c8023ee477ee872d0a7a
dvankeke <dvankeke@akamai.com>
2025-12-01 11:10:04 +0100

    feat: auto image updater

--- src/pages/workloads/create-edit/WorkloadsCreateEditPage.tsx
+++ src/pages/workloads/create-edit/WorkloadsCreateEditPage.tsx
@@ -2,7 +2,7 @@ import { Box, Button, ButtonGroup, Typography } from '@mui/material'
 import YAML from 'yaml'
 import { omit } from 'lodash'
 import React, { useEffect, useState } from 'react'
-import { Resolver, useForm } from 'react-hook-form'
+import { FormProvider, Resolver, useForm } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 import { Redirect, RouteComponentProps, useHistory } from 'react-router-dom'
 import { yupResolver } from '@hookform/resolvers/yup'
@@ -23,6 +23,9 @@ import {
 } from 'redux/otomiApi'
 import DeleteButton from 'components/DeleteButton'
 import { DocsLink } from 'components/DocsLink'
+import ImgButtonGroup from 'components/ImgButtonGroup'
+import { TextField } from 'components/forms/TextField'
+import Section from 'components/Section'
 import CodeEditor from './WorkloadsCodeEditor'
 import { createAplWorkloadApiResponseSchema } from './create-edit-workloads.validator'
 
@@ -65,6 +68,7 @@ export default function WorkloadsCreateEditPage({
 }: RouteComponentProps<Params>): React.ReactElement {
   const { t } = useTranslation()
   const { user } = useSession()
+  const { isPlatformAdmin } = user
   const history = useHistory()
   const dispatch = useAppDispatch()
   const { classes } = useStyles()
@@ -77,8 +81,8 @@ export default function WorkloadsCreateEditPage({
     refetch: refetchWorkload,
   } = useGetAplWorkloadQuery({ teamId, workloadName }, { skip: !workloadName })
 
-  const [createWorkload] = useCreateAplWorkloadMutation()
-  const [updateWorkload] = useEditAplWorkloadMutation()
+  const [createWorkload, { isLoading: isCreating }] = useCreateAplWorkloadMutation()
+  const [updateWorkload, { isLoading: isUpdating }] = useEditAplWorkloadMutation()
   const [deleteWorkload, { isLoading: isLoadingDWL, isSuccess: isSuccessDWL }] = useDeleteAplWorkloadMutation()
 
   const [getWorkloadCatalog, { isLoading: isLoadingCatalog }] = useGetWorkloadCatalogMutation()
@@ -96,7 +100,6 @@ export default function WorkloadsCreateEditPage({
   const mergedDefaultValues = createAplWorkloadApiResponseSchema.cast({
     ...workload,
     kind: workload?.kind ?? 'AplTeamWorkload',
-
     metadata: {
       ...workload?.metadata,
       name: workload?.metadata?.name ?? '',
@@ -105,7 +108,6 @@ export default function WorkloadsCreateEditPage({
         'apl.io/teamId': workload?.metadata?.labels?.['apl.io/teamId'] ?? teamId,
       },
     },
-
     spec: {
       ...workload?.spec,
       url: workload?.spec?.url ?? '',
@@ -116,9 +118,17 @@ export default function WorkloadsCreateEditPage({
   }) as CreateAplWorkloadApiResponse
 
   const methods = useForm<CreateAplWorkloadApiResponse>({
+    disabled: !isPlatformAdmin,
     resolver: yupResolver(createAplWorkloadApiResponseSchema) as Resolver<CreateAplWorkloadApiResponse>,
     defaultValues: mergedDefaultValues,
   })
+
+  const {
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+  } = methods
 
   // Fetch catalog info only when creating (no workloadName)
   useEffect(() => {
@@ -147,16 +157,11 @@ export default function WorkloadsCreateEditPage({
   const valuesData = workloadName ? workload?.spec?.values : catalogItem?.values
   const valuesSchema = catalogItem?.valuesSchema
 
-  // Local state for the inlined Catalog behaviour
-  const [data, setData] = useState<any>(workloadData)
   const [workloadValuesYaml, setWorkloadValuesYaml] = useState(
     typeof valuesData === 'string' ? valuesData : YAML.stringify(valuesData ?? {}),
   )
 
-  // Keep local state in sync when workload/cataIog changes
   useEffect(() => {
-    if (!workloadData) return
-    setData(workloadData)
     setWorkloadValuesYaml(typeof valuesData === 'string' ? valuesData : YAML.stringify(valuesData ?? {}))
   }, [workloadData, valuesData])
 
@@ -167,25 +172,75 @@ export default function WorkloadsCreateEditPage({
     if (!isFetchingWorkload) refetchWorkload()
   }, [isDirty, workloadName, isFetchingWorkload, refetchWorkload])
 
-  const mutating = isLoadingDWL
+  const mutating = isLoadingDWL || isCreating || isUpdating
   if (!mutating && isSuccessDWL) return <Redirect to={`/teams/${teamId}/workloads`} />
 
-  const icon = data?.icon || '/logos/akamai_logo.svg'
+  const icon = workloadData?.icon || '/logos/akamai_logo.svg'
 
-  const handleCreateUpdateWorkload = async () => {
-    const workloadBody = omit(data, ['chartProvider', 'chart', 'revision'])
-    const chartMetadata = omit(data?.chartMetadata, ['helmChartCatalog', 'helmChart'])
-    const path = workloadData?.path
+  // ---- Auto image updater state ----
+  type AutoUpdaterType = 'disabled' | 'digest' | 'semver'
+  const watchedStrategyType = watch('spec.imageUpdateStrategy.type') as AutoUpdaterType | undefined
+  const [autoUpdaterType, setAutoUpdaterType] = useState<AutoUpdaterType>('disabled')
+
+  // initialise local state from form values
+  useEffect(() => {
+    if (watchedStrategyType) setAutoUpdaterType(watchedStrategyType)
+  }, [watchedStrategyType])
+
+  const autoImageUpdaterOptions = [
+    {
+      value: 'disabled',
+      label: 'Disabled',
+      caption: '',
+    },
+    {
+      value: 'digest',
+      label: 'Digest',
+      caption: 'Latest, feat, dev',
+    },
+    {
+      value: 'semver',
+      label: 'Semver',
+      caption: '1.x.x',
+    },
+  ]
+
+  const handleAutoUpdaterChange = (value: string) => {
+    const selected = value as AutoUpdaterType
+    setAutoUpdaterType(selected)
+
+    if (selected === 'disabled') setValue('spec.imageUpdateStrategy', { type: 'disabled' } as any)
+    else if (selected === 'digest') {
+      // keep existing digest block if present
+      const currentDigest = watch('spec.imageUpdateStrategy.digest') ?? {}
+      setValue('spec.imageUpdateStrategy', {
+        type: 'digest',
+        digest: currentDigest,
+      } as any)
+    } else if (selected === 'semver') {
+      const currentSemver = watch('spec.imageUpdateStrategy.semver') ?? {}
+      setValue('spec.imageUpdateStrategy', {
+        type: 'semver',
+        semver: currentSemver,
+      } as any)
+    }
+  }
+
+  const onSubmit = async (formData: CreateAplWorkloadApiResponse) => {
+    const workloadBody = omit(formData.spec, ['chartProvider', 'chart', 'revision'])
+    const chartMetadata = omit(formData.spec?.chartMetadata, ['helmChartCatalog', 'helmChart'])
+    const path = workloadData?.path ?? (formData as any).path
+    const url = workloadData?.url ?? (formData as any).url ?? ''
 
     const body: CreateAplWorkloadApiArg['body'] = {
       kind: 'AplTeamWorkload',
       metadata: {
-        name: workloadName ?? data?.name,
+        name: workloadName ?? formData.metadata?.name ?? '',
       },
       spec: {
         ...workloadBody,
         chartMetadata,
-        url: workloadData?.url,
+        url,
         path,
         values: workloadValuesYaml,
       },
@@ -197,74 +252,120 @@ export default function WorkloadsCreateEditPage({
       res = await updateWorkload({ teamId, workloadName, body })
     } else res = await createWorkload({ teamId, body })
 
-    if (!res.error) history.push(`/teams/${teamId}/workloads`)
+    if (!('error' in res && res.error)) history.push(`/teams/${teamId}/workloads`)
   }
 
-  const comp = !isErrorWorkload && (
-    <Box sx={{ width: '100%' }}>
-      <Box className={classes.header}>
-        <Box className={classes.imgHolder}>
-          <img
-            className={classes.img}
-            src={icon}
-            onError={({ currentTarget }) => {
-              // eslint-disable-next-line no-param-reassign
-              currentTarget.onerror = null
-              // eslint-disable-next-line no-param-reassign
-              currentTarget.src = `${icon}`
-            }}
-            alt={`Logo for ${icon}`}
-          />
-        </Box>
-        <Box>
-          <Typography variant='h6'>
-            {workloadData?.name ? `${workloadData.name} (${workloadData.path})` : workloadData?.path}
-          </Typography>
-        </Box>
-        {workloadData?.url && workloadData?.path && (
-          <Box sx={{ ml: 'auto' }}>
-            <DocsLink href={getDocsLink(workloadData.url as string, workloadData.path as string)} />
-          </Box>
-        )}
-      </Box>
-
-      {/* <Form
-        schema={schema}
-        uiSchema={uiSchema}
-        data={data}
-        onChange={setData}
-        resourceType='Workload'
-        children
-        hideHelp
-        liveValidate={data?.name || data?.namespace}
-        mutating={mutating}
-      /> */}
-
-      <CodeEditor code={workloadValuesYaml} onChange={setWorkloadValuesYaml} validationSchema={valuesSchema} />
-
-      <Box sx={{ display: 'flex', alignItems: 'center', ml: 'auto', float: 'right', mt: 2 }}>
-        <ButtonGroup sx={{ gap: '10px' }}>
-          <Button variant='contained' onClick={handleCreateUpdateWorkload}>
-            Submit
-          </Button>
-          {workloadName && (
-            <DeleteButton
-              onDelete={() => deleteWorkload({ teamId, workloadName })}
-              resourceName={workloadData?.name}
-              resourceType='workload'
-              data-cy='button-delete-workload'
-            />
-          )}
-        </ButtonGroup>
-      </Box>
-    </Box>
-  )
+  if (isLoadingWorkload || isLoadingCatalog) return <PaperLayout loading title={t('TITLE_SERVICE')} />
 
   return (
-    <PaperLayout
-      loading={isLoadingWorkload || isLoadingCatalog}
-      comp={comp}
-      title={t('TITLE_WORKLOAD', { workloadName, role: 'team' })}
-    />
+    <PaperLayout title={t('TITLE_WORKLOAD', { workloadName, role: 'team' })}>
+      {isErrorWorkload ? null : (
+        <FormProvider {...methods}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Box sx={{ width: '100%' }}>
+              <Box className={classes.header}>
+                <Box className={classes.imgHolder}>
+                  <img
+                    className={classes.img}
+                    src={icon}
+                    onError={({ currentTarget }) => {
+                      // eslint-disable-next-line no-param-reassign
+                      currentTarget.onerror = null
+                      // eslint-disable-next-line no-param-reassign
+                      currentTarget.src = `${icon}`
+                    }}
+                    alt={`Logo for ${icon}`}
+                  />
+                </Box>
+                <Box>
+                  <Typography variant='h6'>
+                    {workloadData?.name ? `${workloadData.name} (${workloadData.path})` : workloadData?.path}
+                  </Typography>
+                </Box>
+                {workloadData?.url && workloadData?.path && (
+                  <Box sx={{ ml: 'auto' }}>
+                    <DocsLink href={getDocsLink(workloadData.url as string, workloadData.path as string)} />
+                  </Box>
+                )}
+              </Box>
+
+              <Section>
+                <TextField
+                  label='Workload name'
+                  width='large'
+                  noMarginTop
+                  value={watch('metadata.name') || ''}
+                  onChange={(e) => setValue('metadata.name', e.target.value)}
+                  error={!!errors.metadata?.name}
+                  helperText={errors.metadata?.name?.message?.toString()}
+                  disabled={!!workloadName || !isPlatformAdmin}
+                />
+              </Section>
+
+              {/* Auto image updater */}
+              <Section
+                title='Auto Image Updater'
+                description='Automatically update the image. Only supported when the image is stored in Harbor.'
+              >
+                <ImgButtonGroup
+                  name='spec.imageUpdateStrategy.type'
+                  control={methods.control}
+                  options={autoImageUpdaterOptions}
+                  value={autoUpdaterType}
+                  onChange={handleAutoUpdaterChange}
+                />
+                {autoUpdaterType === 'digest' && (
+                  <Box sx={{ mt: 3, maxWidth: 480 }}>
+                    <TextField
+                      label='Version'
+                      width='large'
+                      noMarginTop
+                      placeholder='latest, feat, dev'
+                      {...methods.register('spec.imageUpdateStrategy.digest.tag' as const)}
+                    />
+                  </Box>
+                )}
+                {autoUpdaterType === 'semver' && (
+                  <Box sx={{ mt: 3, maxWidth: 480 }}>
+                    <TextField
+                      label='Version'
+                      width='large'
+                      noMarginTop
+                      placeholder='1.x.x'
+                      {...methods.register('spec.imageUpdateStrategy.semver.versionConstraint' as const)}
+                    />
+                  </Box>
+                )}
+                {/* {autoUpdaterType === 'disabled' && (
+                  <Box sx={{ mt: 3, maxWidth: 480 }}>
+                    <TextField label='Version' width='large' noMarginTop placeholder='latest, feat, dev' disabled />
+                  </Box>
+                )} */}
+              </Section>
+
+              {/* Values editor */}
+              <CodeEditor code={workloadValuesYaml} onChange={setWorkloadValuesYaml} validationSchema={valuesSchema} />
+
+              <Box sx={{ display: 'flex', alignItems: 'center', ml: 'auto', float: 'right', mt: 2 }}>
+                <ButtonGroup sx={{ gap: '10px' }}>
+                  <Button variant='contained' type='submit' disabled={mutating || !isPlatformAdmin}>
+                    Submit
+                  </Button>
+                  {workloadName && (
+                    <DeleteButton
+                      onDelete={() => deleteWorkload({ teamId, workloadName })}
+                      resourceName={workloadData?.name}
+                      resourceType='workload'
+                      data-cy='button-delete-workload'
+                      disabled={mutating || !isPlatformAdmin}
+                    />
+                  )}
+                </ButtonGroup>
+              </Box>
+            </Box>
+          </form>
+        </FormProvider>
+      )}
+    </PaperLayout>
   )
 }
