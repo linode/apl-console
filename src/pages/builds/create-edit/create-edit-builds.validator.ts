8fc09fbaa0f666d9c1cf3179b7f72252275b2e18
dvankeke <dvankeke@akamai.com>
2026-02-04 09:35:55 +0100

    feat: build v2 endpoint

--- src/pages/builds/create-edit/create-edit-builds.validator.ts
+++ src/pages/builds/create-edit/create-edit-builds.validator.ts
@@ -1,29 +1,23 @@
-import { array, boolean, mixed, object, string } from 'yup'
+import * as yup from 'yup'
 
-const envVarSchema = object({
-  name: string()
+const envVarSchema = yup.object({
+  name: yup
+    .string()
     .required('Environment variable name is required')
     .matches(/^-*[a-zA-Z_][a-zA-Z0-9_-]*$/, 'Invalid environment variable name'),
-  value: string().optional(),
+  value: yup.string().optional(),
 })
 
-const commonModeSchema = object({
-  repoUrl: string().required('Repository URL is required').url('Invalid repository URL'),
-  path: string().optional(),
-  revision: string().optional(),
-  envVars: array().of(envVarSchema).optional(),
+const commonModeSchema = yup.object({
+  repoUrl: yup.string().required('Repository URL is required').url('Invalid repository URL'),
+  path: yup.string().optional(),
+  revision: yup.string().optional(),
+  envVars: yup.array().of(envVarSchema).optional(),
 })
 
-export const buildApiResponseSchema = object({
-  id: string().optional(),
-  teamId: string().optional(),
-  name: string()
-    .optional()
-    .matches(
-      /^[a-z]([-a-z0-9]*[a-z0-9])+$/,
-      'Invalid format, must start with a lowercase letter, contain only lowercase letters, numbers, or hyphens, and end with a letter or number.',
-    ),
-  imageName: string()
+const specSchema = yup.object({
+  imageName: yup
+    .string()
     .required('Image name is required')
     .matches(/^[a-z0-9]([-a-z0-9]*[a-z0-9])?$/, 'Image name can only contain lowercase letters, numbers, and hyphens.')
     .test(
@@ -41,17 +35,17 @@ export const buildApiResponseSchema = object({
       'is-unique',
       'Container image name already exists, the combined image name and tag must be unique.',
       function (value) {
-        const { buildNames, validateOnSubmit } = this.options.context || {}
-        // Only validate uniqueness if `validateOnSubmit` is true
+        const { buildNames, validateOnSubmit } = (this.options.context || {}) as any
         if (!validateOnSubmit) return true
         const { tag } = this.parent
         const expectedBuildName = `${value}-${tag}`
-        return !buildNames.some((name) => name === expectedBuildName)
+        return !buildNames?.some((name: string) => name === expectedBuildName)
       },
     ),
-  tag: string()
+
+  tag: yup
+    .string()
     .required('Tag is required')
-    // https://pkg.go.dev/github.com/distribution/reference#pkg-overview
     .matches(
       /^[\w][\w.-]{0,127}$/,
       'Tag must start with a letter, digit, or underscore, and can include dots, hyphens, underscores.',
@@ -62,39 +56,70 @@ export const buildApiResponseSchema = object({
       return expectedBuildName.length <= 128
     })
     .test('is-unique', '', function (value) {
-      const { buildNames, validateOnSubmit } = this.options.context || {}
-      // Only validate uniqueness if `validateOnSubmit` is true
+      const { buildNames, validateOnSubmit } = (this.options.context || {}) as any
       if (!validateOnSubmit) return true
       const { imageName } = this.parent
       const expectedBuildName = `${imageName}-${value}`
-      return !buildNames.some((name) => name === expectedBuildName)
+      return !buildNames?.some((name: string) => name === expectedBuildName)
     }),
-  mode: object({
-    type: string().required('Mode type is required').oneOf(['docker', 'buildpacks'], 'Invalid mode type'),
-    docker: mixed().when('type', {
-      is: 'docker',
-      then: commonModeSchema.required('Docker configuration is required'),
-    }),
-    buildpacks: mixed().when('type', {
-      is: 'buildpacks',
-      then: commonModeSchema
-        .required('Buildpacks configuration is required')
-        .test('Does not contain "./"', '', function (value) {
-          const { path } = value
-          if (path && path.includes('./')) {
-            return this.createError({
-              path: 'mode.buildpacks.path',
-              message: 'Path for Buildpacks cannot contain "./"',
-            })
-          }
 
-          return true
-        }),
+  mode: yup
+    .object({
+      type: yup.string().required('Mode type is required').oneOf(['docker', 'buildpacks'], 'Invalid mode type'),
+
+      docker: yup.mixed().when('type', {
+        is: 'docker',
+        then: commonModeSchema.required('Docker configuration is required'),
+      }),
+
+      buildpacks: yup.mixed().when('type', {
+        is: 'buildpacks',
+        then: commonModeSchema
+          .required('Buildpacks configuration is required')
+          .test('Does not contain "./"', '', function (value: any) {
+            const { path } = value || {}
+            if (path && path.includes('./')) {
+              return this.createError({
+                path: 'spec.mode.buildpacks.path',
+                message: 'Path for Buildpacks cannot contain "./"',
+              })
+            }
+            return true
+          }),
+      }),
+    })
+    .required('Mode configuration is required'),
+
+  externalRepo: yup.boolean().optional(),
+  secretName: yup.string().min(2, 'Secret name must be at least 2 characters long.').optional(),
+  trigger: yup.boolean().optional(),
+  scanSource: yup.boolean().optional(),
+})
+
+export const aplBuildApiSchema = yup.object({
+  kind: yup.string().oneOf(['AplTeamBuild']).required(),
+
+  spec: specSchema.required(),
+
+  metadata: yup.object({
+    name: yup.string().optional(),
+    labels: yup.object({
+      'apl.io/teamId': yup.string().required(),
     }),
-  }).required('Mode configuration is required'),
-  codeRepoName: string().optional(),
-  externalRepo: boolean().optional(),
-  secretName: string().min(2, 'Secret name must be at least 2 characters long.').optional(),
-  trigger: boolean().optional(),
-  scanSource: boolean().optional(),
+  }),
+
+  status: yup.object({
+    conditions: yup
+      .array(
+        yup.object({
+          lastTransitionTime: yup.string().optional(),
+          message: yup.string().optional(),
+          reason: yup.string().optional(),
+          status: yup.boolean().optional(),
+          type: yup.string().optional(),
+        }),
+      )
+      .optional(),
+    phase: yup.string().optional(),
+  }),
 })
