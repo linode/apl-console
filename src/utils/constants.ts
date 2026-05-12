0b2de0f8fa4d78db2b206d5aad9761be2b0db1a0
dvankeke <dvankeke@akamai.com>
2026-05-12 09:11:34 +0200

    fix: git agnostic safe url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
 export const SAFE_REPO_URL =
-  /^(github\.com|gitlab\.com|[^/\s]+\.gitea[^/\s]*)\/[A-Za-z0-9_.-]+\/[A-Za-z0-9_.-]+(?:\.git)?$/
+  /^(?:https:\/\/[A-Za-z0-9.-]+\.[A-Za-z]{2,}\/|git@[A-Za-z0-9.-]+\.[A-Za-z]{2,}:|[A-Za-z0-9.-]+\.[A-Za-z]{2,}\/)[A-Za-z0-9_.-]+(?:\/[A-Za-z0-9_.-]+)+(?:\.git)?$/
